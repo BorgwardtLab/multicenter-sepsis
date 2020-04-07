b70221698e1ac6f6520754bfcf8c136e4cf7d59e
mi92 <michael.d.moor@gmail.com>
2020-04-07 10:08:08 +0200

    added extended_ts_columns to extracted.py (which gatheres convenience variables0

--- src/sklearn/data/extracted.py
+++ src/sklearn/data/extracted.py
@@ -45,6 +45,18 @@ ts_columns = [
         'WBC', 'Fibrinogen', 'Platelets'
     ]
 
+#adding derived features (which are still time series -- not yet features capturing temporal information!)
+extended_ts_columns = [
+        'HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'EtCO2',
+        'BaseExcess', 'HCO3', 'FiO2', 'pH', 'PaCO2', 'SaO2', 'AST', 'BUN',
+        'Alkalinephos', 'Calcium', 'Chloride', 'Creatinine',
+        'Bilirubin_direct', 'Glucose', 'Lactate', 'Magnesium', 'Phosphate',
+        'Potassium', 'Bilirubin_total', 'TroponinI', 'Hct', 'Hgb', 'PTT',
+        'WBC', 'Fibrinogen', 'Platelets',
+        'ShockIndex', 'BUN/CR', 'SaO2/FiO2', 'SOFA', 'SOFA_deterioration', 
+        'sofa_max_24hrs', 'qSOFA', 'SepticShock', 'MEWS', 'SIRS'      
+    ]
+
 columns_not_to_normalize = [ 
     'Gender', 'Unit1', 'Unit2',
     'HospAdmTime', 'ICULOS'
