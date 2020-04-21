75fab0fc19a5f002e0200c7cea1218ea493be5fc
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-04-21 21:19:06 +0200

    move challenge data to r preproc

--- r/create_dataset.R
+++ r/create_dataset.R
@@ -2,6 +2,8 @@
 
 library(optparse)
 library(ricu)
+library(readr)
+library(data.table)
 
 challenge_map <- data.frame(
   rbind(
@@ -50,6 +52,50 @@ challenge_map <- data.frame(
   stringsAsFactors = FALSE
 )
 
+challenge_spec <- cols(
+  HR = col_double(),
+  O2Sat = col_double(),
+  Temp = col_double(),
+  SBP = col_double(),
+  MAP = col_double(),
+  DBP = col_double(),
+  Resp = col_double(),
+  EtCO2 = col_double(),
+  BaseExcess = col_double(),
+  HCO3 = col_double(),
+  FiO2 = col_double(),
+  pH = col_double(),
+  PaCO2 = col_double(),
+  SaO2 = col_double(),
+  AST = col_double(),
+  BUN = col_double(),
+  Alkalinephos = col_double(),
+  Calcium = col_double(),
+  Chloride = col_double(),
+  Creatinine = col_double(),
+  Bilirubin_direct = col_double(),
+  Glucose = col_double(),
+  Lactate = col_double(),
+  Magnesium = col_double(),
+  Phosphate = col_double(),
+  Potassium = col_double(),
+  Bilirubin_total = col_double(),
+  TroponinI = col_double(),
+  Hct = col_double(),
+  Hgb = col_double(),
+  PTT = col_double(),
+  WBC = col_double(),
+  Fibrinogen = col_double(),
+  Platelets = col_double(),
+  Age = col_integer(),
+  Gender = col_integer(),
+  Unit1 = col_integer(),
+  Unit2 = col_integer(),
+  HospAdmTime = col_double(),
+  ICULOS = col_integer(),
+  SepsisLabel = col_logical()
+)
+
 sepsis3_score <- function(source, pids = NULL) {
 
   sofa_score <- sofa(source, id_type = "icustay", patient_ids = pids)
@@ -74,33 +120,75 @@ sepsis3_score <- function(source, pids = NULL) {
 
 dump_dataset <- function(source = "mimic_demo", dir = tempdir()) {
 
-  dat <- load_dictionary(source, challenge_map[[2L]], id_type = "icustay")
-  ids <- unique(dat[age >= 14, id(dat), with = FALSE])
-  dat <- merge(dat, ids, all.y = TRUE)
+  if (identical(source, "challenge")) {
 
-  sep3 <- sepsis3_score(source, ids)
-  sep3 <- data.table::set(sep3, j = "SepsisLabel", value = 1L)
+    data_dir <- file.path(dir, "training_setB")
 
-  dat <- rename_cols(dat, challenge_map[[1L]], challenge_map[[2L]],
-                     skip_absent = TRUE)
+    stopifnot(dir.exists(data_dir))
 
-  res <- merge(dat, sep3[, c(meta_cols(sep3), "SepsisLabel"), with = FALSE],
-               all.x = TRUE)
-  res <- res[, c("SepsisLabel") := data.table::nafill(SepsisLabel, "locf"),
+    dat <- read_psv(data_dir, col_spec = challenge_spec, id_col = "ID")
+
+    dat <- dat[, Gender := fifelse(Gender == 0L, "Female", "Male")]
+    dat <- dat[, O2Sat := rowMeans(.SD, na.rm=TRUE),
+               .SDcols = c("O2Sat", "SaO2")]
+    dat <- dat[, ICULOS := as.difftime(ICULOS, units = "hours")]
+    dat <- as_ts_tbl(dat, "ID")
+
+    sep <- dat[(SepsisLabel), .(ICULOS = min(ICULOS) + 6), by = "ID"]
+    sep <- rename_cols(sep, "sep3_time", "ICULOS")
+
+    dat <- rm_cols(dat, "SepsisLabel")
+
+  } else {
+
+    dat <- load_dictionary(source, challenge_map[[2L]], id_type = "icustay")
+    ids <- unique(dat[age >= 14, id(dat), with = FALSE])
+    dat <- merge(dat, ids, all.y = TRUE)
+
+    dat <- rename_cols(dat, c("ICULOS", challenge_map[[1L]]),
+                            c(index(dat), challenge_map[[2L]]),
+                       skip_absent = TRUE)
+
+    win <- stay_windows(source, id_type = "icustay", win_type = "icustay",
+                        in_time = "intime", out_time = "outtime")
+
+    dat  <- dat[, c("join_time") := list(get(index(dat)))]
+
+    join <- c(paste(id(dat), "==", id(win)), "join_time <= outtime")
+    dat <- dat[win, on = join]
+    dat <- rm_cols(dat, c("join_time", "intime"))
+
+    sep <- sepsis3_score(source, ids)
+    sep  <- sep[, c("join_time") := list(get(index(sep)))]
+
+    join <- c(paste(id(sep), "==", id(win)), "join_time >= intime",
+                                             "join_time <= outtime")
+    sep <- sep[win, on = join]
+    sep <- rm_cols(sep, data_cols(sep))
+  }
+
+  sep <- set(sep, j = "SepsisLabel", value = 1L)
+
+  res <- merge(dat, sep, all.x = TRUE)
+  res <- res[, c("SepsisLabel") := nafill(SepsisLabel, "locf"),
              by = c(id(res))]
-  res <- res[, c("SepsisLabel") := data.table::nafill(SepsisLabel, fill = 0L)]
+  res <- res[, c("SepsisLabel") := nafill(SepsisLabel, fill = 0L)]
 
-  wins <- stay_windows(source, id_type = "icustay", win_type = "icustay",
-                       in_time = "intime", out_time = "outtime")
-  res  <- res[, c("join_time") := list(get(index(res)))]
+  res <- rm_cols(res, setdiff(data_cols(res), challenge_map[[1L]]))
 
-  join <- c(paste(id(res), "==", id(wins)), "join_time <= outtime")
-  res <- res[wins, on = join]
-  res <- rm_cols(res, c("join_time", "intime"))
+  miss_cols <- setdiff(challenge_map[[1L]], data_cols(res))
+
+  if (length(miss_cols)) {
+    res <- set(res, j = miss_cols, value = NA_real_)
+  }
+
+  res <- setcolorder(res, c(meta_cols(res), challenge_map[[1L]]))
 
   dir <- file.path(dir, source)
 
-  if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+  if (dir.exists(dir)) unlink(dir, recursive = TRUE)
+
+  dir.create(dir, recursive = TRUE)
 
   write_psv(res, dir, na_rows = TRUE)
 }
@@ -133,7 +221,7 @@ demo <- c("mimic_demo", "eicu_demo")
 prod <- c("mimic", "eicu", "hirid")
 all  <- c(demo, prod)
 
-data_opts <- c(all, "demo", "production", "all")
+data_opts <- c(all, "demo", "production", "all", "challenge")
 
 if (!(length(opt$src) == 1L && opt$src %in% data_opts)) {
   cat("\nSelect a data source among the following options:\n  ",
@@ -162,4 +250,6 @@ if (identical(opt$src, "all")) {
 for (src in sources) {
   message("dumping `", src, "`")
   dump_dataset(source = src, dir = opt$path)
+  dir <- file.path(opt$path, src)
+  tar(paste0(dir, ".tar.gz"), dir, "gzip")
 }
