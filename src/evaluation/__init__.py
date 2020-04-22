1d178372bb5034c76788511fed00e14c9ced1205
Max Horn <max.horn@bsse.ethz.ch>
2020-04-22 16:50:35 +0200

    Add wrapped physionet score to eval module

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,1 +1,10 @@
+"""Evaluation scores and utilities."""
 from .physionet2019_score import physionet2019_utility
+from .sklearn_utils import OnlineScoreWrapper, StratifiedPatientKFold
+from sklearn.metrics import make_scorer
+
+physionet2019_scorer = make_scorer(OnlineScoreWrapper(physionet2019_utility))
+
+__all__ = [
+    'physionet2019_utility', 'physionet2019_scorer',
+    'OnlineScoreWrapper', 'StratifiedPatientKFold']
