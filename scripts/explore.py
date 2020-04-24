345a38a5dce6ccaed53b9f412f61e3b9fc42fcd2
mi92 <michael.d.moor@gmail.com>
2020-04-24 12:10:32 +0200

    added split argument to explore script

--- scripts/explore.py
+++ scripts/explore.py
@@ -14,16 +14,20 @@ if __name__ == "__main__":
         default='data/sklearn/processed')
     parser.add_argument('--dataset', type=str, help='dataset to use', 
         default='physionet2019')
+    parser.add_argument('--split', type=str, help='split to use', 
+        default='validation')
 
     args = parser.parse_args()
-    
+    split = args.split 
     path = os.path.join('datasets', args.dataset, args.path)
-    normalized_path = os.path.join(path, 'X_normalized_train.pkl')
-    features_path = os.path.join(path, 'X_features_train.pkl')
-    y_path = os.path.join(path, 'y_train.pkl')
-
-    df = load_pickle(normalized_path)
+    raw_data_path = os.path.join(path, f'raw_data_{split}.pkl')
+    normalized_path = os.path.join(path, f'X_normalized_{split}.pkl')
+    features_path = os.path.join(path, f'X_features_{split}.pkl')
+    y_path = os.path.join(path, f'y_{split}.pkl')
+    raw_y_path =  os.path.join(path, f'raw_y_{split}.pkl')
+    df = load_pickle(raw_data_path)
+    df_n = load_pickle(normalized_path)
     X = load_pickle(features_path)
     y = load_pickle(y_path)
-
+    raw_y = load_pickle(raw_y_path)
     embed()
