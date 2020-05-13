33d1d7469432af956ec9f578cfb236d97ab29931
mi92 <michael.d.moor@gmail.com>
2020-05-13 15:27:22 +0200

    added shell script to run preprocessing of all datasets (and demo)

---
+++ scripts/run_preprocessing.sh
@@ -0,0 +1,6 @@
+python src/sklearn/data/make_dataframe.py --dataset demo --n_jobs=10 --n_partitions=20 --overwrite
+python src/sklearn/data/make_dataframe.py --dataset mimic3 --n_jobs=50 --n_partitions=20000 --overwrite
+python src/sklearn/data/make_dataframe.py --dataset hirid --n_jobs=50 --n_partitions=20000 --overwrite
+python src/sklearn/data/make_dataframe.py --dataset physionet2019 --n_jobs=50 --n_partitions=10000 --overwrite
+python src/sklearn/data/make_dataframe.py --dataset eicu --n_jobs=50 --n_partitions=40000 --overwrite
+
