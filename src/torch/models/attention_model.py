719c0034e3459ec5f7772756d6997636b095636c
Max Horn <max.horn@bsse.ethz.ch>
2020-07-30 18:03:01 +0200

    Move to torch implementation of Transformer and implement ReZero
    Closes #4

--- src/torch/models/attention_model.py
+++ src/torch/models/attention_model.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from src.torch.models.base_model import BaseModel
 from src.torch.torch_utils import PositionalEncoding, to_observation_tuples
 
@@ -33,7 +34,7 @@ class BatchNorm(nn.Module):
     def __init__(self, nf, mom=0.1, eps=1e-5):
         super().__init__()
         # NB: pytorch bn mom is opposite of what you'd expect
-        self.mom,self.eps = mom,eps
+        self.mom, self.eps = mom,eps
         self.mults = nn.Parameter(torch.ones (1, 1, nf))
         self.adds  = nn.Parameter(torch.zeros(1, 1, nf))
         self.register_buffer('vars',  torch.ones(1, 1,nf))
@@ -69,142 +70,13 @@ class MaskedLayerNorm(nn.LayerNorm):
         return ((x - mean_x) / std_x) * self.weight + self.bias
 
 
-# pylint: disable=R0902,C0103,W0221
-class MultiHeadAttention(nn.Module):
-    """Multi head attention layer."""
-
-    def __init__(self, d_model, n_heads, d_k, d_v, dropout, mask_future=True):
-        """Multi head attention layer.
-
-        Args:
-            d_model: Embedding dimensionality
-            n_heads: Number of attention heads
-            qkd_v: Dimensionality of the q, k and v vectors
-            mask_future: When predicting for a time point mask all future
-                events
-        """
+class ReZero(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.d_model = d_model
-        self.n_heads = n_heads
-        self.d_k = d_k
-        self.d_v = d_v
-        self.mask_future = mask_future
+        self.resweight = nn.Parameter(torch.Tensor([0]))
 
-        self.w_q = nn.Linear(d_model, n_heads * d_k)
-        self.w_k = nn.Linear(d_model, n_heads * d_k)
-        self.w_v = nn.Linear(d_model, n_heads * d_v)
-        self.attention = ScaledDotProductAttention(
-            np.sqrt(d_k), dropout=dropout)
-        self.w_o = nn.Linear(n_heads * d_v, d_model)
-
-    def compute_mask(self, x):
-        return get_subsequent_mask(x)
-
-    def forward(self, x):
-        """Apply multi head attention to input data x.
-
-        Args:
-            x: Tensor with [bs x time points x d_model]
-
-        Returns:
-            Transformed tensor
-
-        """
-        n_heads, d_k, d_v = self.n_heads, self.d_k, self.d_v
-        bs, length, _ = x.size()
-
-        q = self.w_q(x).view(bs, length, n_heads, d_k)
-        k = self.w_k(x).view(bs, length, n_heads, d_k)
-        v = self.w_v(x).view(bs, length, n_heads, d_v)
-
-        # Combine heads and batch dimension.  Use chained continguous and view
-        # to ensure we are not doing anything wrong.
-        q = q.permute(2, 0, 1, 3).contiguous().view(bs*n_heads, length, d_k)
-        k = k.permute(2, 0, 1, 3).contiguous().view(bs*n_heads, length, d_k)
-        v = v.permute(2, 0, 1, 3).contiguous().view(bs*n_heads, length, d_v)
-
-        mask = self.compute_mask(x) if self.mask_future else None
-
-        z_flat, attention = self.attention(q, k, v, mask=mask)
-
-        # Undo combination of heads and batch
-        z = z_flat.view(n_heads, bs, length, d_v)
-        z = z.permute(1, 2, 0, 3).contiguous()
-        # Combine outputs from heads into single vector
-        z = z.view(bs, length, n_heads*d_v)
-
-        output = self.w_o(z)
-        return output
-
-
-class ScaledDotProductAttention(nn.Module):
-    """Scaled dot product attention from `Attention is all you need` paper."""
-
-    def __init__(self, scale, dropout=0.1):
-        """Scaled dot product attention.
-
-        Args:
-            scale: Scaling applied prior to softmax
-            dropout: Fraction of values randomly dropped out from attention
-        """
-        super().__init__()
-        self.scale = scale
-        self.softmax = nn.Softmax(dim=2)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, q, k, v, mask=None):
-        """Apply dot product attention.
-
-        All inputs are assumed to be 3d tensors.
-
-        Args:
-            q: Query [bs*n_heads x n_instances x dq]
-            k: Keys [bs*n_heads x n_instances x dq]
-            v: Values [bs*n_heads x n_instances x dq]
-            mask: Broadcastable with [bs*n_heads x n_instances x n_instances]
-
-        Returns: Output of attention operation and attention values
-        """
-        attention = torch.bmm(q, k.transpose(1, 2))
-        attention = attention/self.scale
-
-        if mask is not None:
-            # -inf is ignored by the softmax function
-            attention.masked_fill_(~mask, -np.inf)
-
-        attention = self.softmax(attention)
-        out = torch.bmm(self.dropout(attention), v)
-
-        return out, attention
-
-
-class PositionwiseFeedForward(nn.Module):
-    """A two layer feed forward module."""
-
-    def __init__(self, d_in, d_hid, dropout=0.1):
-        """Position-wise feed-forward layer with residual.
-
-        Applies two a two-layer neural network to the axis -2 of the input.
-
-        Args:
-            d_in: Dimensionality of input and output
-            d_hid: Dimensionality of intermediate representation
-            dropout: Fraction of nodes to randomly drop out
-        """
-        super().__init__()
-        self.w_1 = nn.Conv1d(d_in, d_hid, 1)  # position-wise
-        self.w_2 = nn.Conv1d(d_hid, d_in, 1)  # position-wise
-        self.layer_norm = BatchNorm(d_in)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x, lengths):
-        residual = x
-        output = x.transpose(-1, -2)
-        output = self.w_2(F.relu(self.w_1(output)))
-        output = output.transpose(-1, -2)
-        output = self.dropout(output)
-        output = self.layer_norm(output + residual, lengths)
-        return output
+    def forward(self, x1, x2):
+        return x1 + self.resweight * x2
 
 
 class PositionwiseLinear(nn.Module):
@@ -226,50 +98,94 @@ class PositionwiseLinear(nn.Module):
 
     def forward(self, x):
         """Apply linear projection to axis -2 of input x."""
-        output = x.transpose(-1, -2)
-        output = self.w_1(output).transpose(-1, -2)
-        return output
+        x = x.permute(1, 2, 0)
+        x = self.w_1(x).permute(2, 0, 1)
+        return x
 
 
-class AttentionLayer(nn.Module):
-    """Attention block with two MultiHeadAttention layers.
+class TransformerEncoderLayer(nn.Module):
+    """TransformerEncoderLayer is made up of self-attn and feedforward network.
+    This standard encoder layer is based on the paper "Attention Is All You
+    Need".  Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion
+    Jones, Aidan N Gomez, Lukasz Kaiser, and Illia Polosukhin. 2017. Attention
+    is all you need. In Advances in Neural Information Processing Systems,
+    pages 6000-6010. Users may modify or implement in a different way during
+    application.
 
-    One block consists of the following Sub-Layers:
-        - MultiHeadAttention
-        - LayerNorm
-        - PositionwiseFeedForward
-        - MultiHeadAttention
-        - LayerNorm
+    This class is adapted from the pytorch source code.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model
+            (default=2048).
+        dropout: the dropout value (default=0.1).
+        norm: Normalization to apply, one of 'layer' or 'rezero'.
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = encoder_layer(src)
     """
 
-    def __init__(self, d_model, n_heads, qkv_dim, ff_hidden_dim=128,
-                 dropout=0.1):
-        """Attention block.
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
+                 activation="relu", norm='layer'):
+        super(TransformerEncoderLayer, self).__init__()
+        if norm == 'layer':
+            def get_residual():
+                def residual(x1, x2):
+                    return x1 + x2
+                return residual
+
+            def get_norm():
+                return nn.LayerNorm(d_model)
+        elif norm == 'rezero':
+            def get_residual():
+                return ReZero()
+            def get_norm():
+                return nn.Identity()
+        else:
+            raise ValueError('Invalid normalization: {}'.format(norm))
+
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = get_norm()
+        self.norm2 = get_norm()
+        self.residual1 = get_residual()
+        self.residual2 = get_residual()
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = F.relu
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(TransformerEncoderLayer, self).__setstate__(state)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        r"""Pass the input through the encoder layer.
 
         Args:
-            d_model: Dimensionality of the model
-            n_heads: Number of attention heads
-            qkd_v: Dimensionality of the q, k and v vectors
-            ff_hidden_dim: Number of hidden units for PositionwiseFeedForward
+            src: the sequence to the encoder layer (required).
+            src_mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
         """
-        super().__init__()
-        self.layers = nn.ModuleList([
-            MultiHeadAttention(d_model, n_heads, qkv_dim, qkv_dim, dropout),
-            BatchNorm(d_model),
-            PositionwiseFeedForward(d_model, ff_hidden_dim),
-            BatchNorm(d_model)
-        ])
-
-    def forward(self, x, lengths):
-        """Apply this attention block to the input x."""
-        out = x
-        for layer in self.layers:
-            if isinstance(layer, (BatchNorm, PositionwiseFeedForward)):
-                out = layer(out, lengths)
-            else:
-                out = layer(out)
-        return out
-
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = self.residual1(src, self.dropout1(src2))
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+        src = self.residual2(src, self.dropout2(src2))
+        src = self.norm2(src)
+        return src
 
 class AttentionModel(BaseModel):
     """Sequence to sequence model based on MultiHeadAttention."""
@@ -280,21 +196,25 @@ class AttentionModel(BaseModel):
         Args:
             d_in: Dimensionality of a single time point
             d_model: Dimensionality of the model
+            ff_dim: Dimensionality of ff layers
             n_layers: Number of MultiHeadAttention layers
             n_heads: Number of attention heads
-            qkd_v: Dimensionality of the q, k and v vectors
         """
         super().__init__(hparams)
         d_model = hparams.d_model
         n_layers = hparams.n_layers
         n_heads = hparams.n_heads
-        qkv_dim = hparams.qkv_dim
+        ff_dim = hparams.ff_dim
         dropout = hparams.dropout
+        norm = hparams.norm
         d_in = self._get_input_dim()
         self.layers = nn.ModuleList(
             [PositionwiseLinear(d_in, d_model)]
-            + [AttentionLayer(d_model, n_heads, qkv_dim, dropout=dropout)
-               for i in range(n_layers)]
+            + [
+                TransformerEncoderLayer(
+                    d_model, n_heads, ff_dim, dropout, norm=norm)
+                for n in range(n_layers)
+            ]
             + [PositionwiseLinear(d_model, 1)]
         )
 
@@ -310,13 +230,12 @@ class AttentionModel(BaseModel):
 
     def forward(self, x, lengths):
         """Apply attention model to input x."""
-        out = x
+        # Invert mask as multi head attention ignores values which are true
+        mask = ~length_to_mask(lengths)
+        out = x.permute(1, 0, 2)
         for layer in self.layers:
-            if isinstance(layer, (AttentionLayer, PositionwiseFeedForward)):
-                out = layer(out, lengths)
-            else:
-                out = layer(out)
-        return out
+            out = layer(out)
+        return out.permute(1, 0, 2)
 
     def optimizer_step(self, epoch_nb, batch_nb, optimizer, optimizer_i, opt_closure):
         n_warmup = 1000.
@@ -333,8 +252,8 @@ class AttentionModel(BaseModel):
         parser = super().add_model_specific_args(parent_parser)
         # MODEL specific
         parser.opt_list(
-            '--d-model', type=int, default=64,
-            tunable=True, options=[64, 128, 256, 512]
+            '--d-model', type=int, default=128,
+            tunable=True, options=[128, 256, 512]
         )
         parser.opt_list(
             '--n-layers', type=int, default=1,
@@ -345,9 +264,11 @@ class AttentionModel(BaseModel):
             tunable=True, options=[4, 8, 16, 32]
         )
         parser.opt_list(
-            '--qkv-dim', type=int, default=32,
-            tunable=True, options=[32, 64, 128]
+            '--ff-dim', type=int, default=32,
+            tunable=True, options=[128, 256, 512, 1028]
         )
+        parser.add_argument(
+            '--norm', type=str, default='rezero', choices=['layer', 'rezero'])
         parser.opt_list(
             '--dropout', default=0.1, type=float,
             tunable=True, options=[0., 0.1, 0.2, 0.3, 0.4]
