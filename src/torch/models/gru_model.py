b899434520f568a2b009ac942eb8e4d04e84e4cd
Max Horn <max.horn@bsse.ethz.ch>
2020-07-30 15:23:43 +0200

    Fix GRU implementation (closes #3)
     - Use to observation tuples transform to mask nans
     - Unpack output of rnn to match labels

--- src/torch/models/gru_model.py
+++ src/torch/models/gru_model.py
@@ -1,53 +1,64 @@
 """Attention based model for sepsis."""
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from src.torch.models.base_model import BaseModel
+from src.torch.torch_utils import to_observation_tuples
 
 
 class GRUModel(BaseModel):
-    """GRU Model"""
+    """GRU Model."""
 
     def __init__(self, hparams):
         """GRUModel.
 
         Args:
-            
+            d_model: Dimensionality of hidden state
+            n_layers: Number of stacked GRU layers
+            dropout: Fraction of elements that should be dropped out
         """
         super().__init__(hparams)
         d_model = hparams.d_model
         n_layers = hparams.n_layers
         dropout = hparams.dropout
         d_in = self._get_input_dim()
-        
-        kwargs = {'hidden_size': d_model, 'input_size': d_in}
-        kwargs['batch_first'] = True
-        kwargs['bidirectional'] = False
-        kwargs['num_layers'] = n_layers
 
-        self.gru = nn.GRU(**kwargs)
-        self.linear = nn.Linear(d_model, 1)      
-   
+        self.gru = nn.GRU(
+            hidden_size=d_model,
+            input_size=d_in,
+            num_layers=n_layers,
+            dropout=dropout,
+            batch_first=True)
+        self.linear = nn.Linear(d_model, 1)
+
     @property
     def transforms(self):
         parent_transforms = super().transforms
+        parent_transforms.append(
+            to_observation_tuples  # mask nan with zero and add indicator
+        )
         return parent_transforms
 
-    # came until here.. 
     def forward(self, x, lengths):
         """Apply GRU model to input x.
             - x: is a three dimensional tensor (batch, stream, channel)
-            - lengths: is a one dimensional tensor (batch,) giving the true length of each batch element along the
-                stream dimension
+            - lengths: is a one dimensional tensor (batch,) giving the true
+              length of each batch element along the stream dimension
         """
-        x = nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
-        #for starters we work with last hidden state, could also use last output or concat
-        _, x = self.gru(x)
-        x = x[-1]  # take the last GRU layer
-        x = self.linear(x)
+        # Convert padded sequence to packed sequence for increased efficiency
+        x = nn.utils.rnn.pack_padded_sequence(
+            x, lengths, batch_first=True, enforce_sorted=False)
+        x, hidden_states = self.gru(x)
+        x = torch.nn.utils.rnn.PackedSequence(
+            self.linear(x.data),
+            x.batch_sizes,
+            x.sorted_indices,
+            x.unsorted_indices
+        )
+        # Unpack sequence to padded tensor for subsequent operations
+        x, lenghts = torch.nn.utils.rnn.pad_packed_sequence(
+            x, batch_first=True)
         return x
-  
+
     @classmethod
     def add_model_specific_args(cls, parent_parser):
         parser = super().add_model_specific_args(parent_parser)
