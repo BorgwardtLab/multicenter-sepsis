cce08d3c970e318573020231bc6d06bac6823baf
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-08-13 10:01:53 +0200

    update data dumping scripts to newes ricu

--- r/utils.R
+++ r/utils.R
@@ -1,38 +1,42 @@
 
-sepsis3_score <- function(source, pids = NULL) {
+sepsis3_crit <- function(source, pids = NULL) {
 
-  sofa_score <- sofa(source, id_type = "icustay", patient_ids = pids)
+  sofa <- load_concepts("sofa_score", source, id_type = "icustay",
+                        patient_ids = pids)
 
   if (grepl("eicu", source)) {
 
-    susp_infec <- si(source, abx_min_count = 2L, positive_cultures = TRUE,
-                     id_type = "icustay", patient_ids = pids, si_mode = "or")
+    si <- load_concepts("susp_inf", source, abx_min_count = 2L,
+                        positive_cultures = TRUE, id_type = "icustay",
+                        patient_ids = pids, si_mode = "or")
 
   } else if (identical(source, "hirid")) {
 
-    susp_infec <- si(source, abx_min_count = 2L, id_type = "icustay",
-                     patient_ids = pids, si_mode = "or")
+    si <- load_concepts("susp_inf", source, abx_min_count = 2L,
+                        id_type = "icustay", patient_ids = pids,
+                        si_mode = "or")
 
   } else {
 
-    susp_infec <- si(source, id_type = "icustay", patient_ids = pids)
+    si <- load_concepts("susp_inf", source, id_type = "icustay",
+                        patient_ids = pids)
   }
 
-  sepsis_3(sofa_score, susp_infec)
+  sepsis_3(sofa, si)
 }
 
-cohort <- function(source) {
+cohort <- function(source, min_age = 14) {
 
-  res <- load_dictionary(source, "age", id_type = "icustay")
-  res <- res[age > 14, ]
+  res <- load_concepts("age", source, id_type = "icustay")
+  res <- res[age > min_age, ]
 
   if (grepl("eicu", source)) {
-    hosp <- data_id(source, "patient", cols = c(id(res), "hospitalid"))
+    hosp <- load_id("patient", source, cols = c(id_var(res), "hospitalid"))
     hosp <- hosp[hospitalid %in% eicu_hospitals, ]
     res <- merge(res, hosp)
   }
 
-  res[[id(res)]]
+  id_col(res)
 }
 
 dump_dataset <- function(source = "mimic_demo", dir = tempdir()) {
@@ -45,70 +49,85 @@ dump_dataset <- function(source = "mimic_demo", dir = tempdir()) {
       stop("need directory ", data_dir, " to continue")
     }
 
-    dat <- read_psv(data_dir, col_spec = challenge_spec, id_col = "ID")
+    feat_map <- concepts[!is.na(concepts[["callenge"]]), ]
+    feats <- do.call(readr::cols,
+      setNames(feat_map[["col_spec"]], feat_map[["callenge"]])
+    )
+
+    dat <- read_psv(data_dir, col_spec = feats, id_var = "stay_id")
 
     dat <- dat[, Gender := data.table::fifelse(Gender == 0L, "Female", "Male")]
     dat <- dat[, O2Sat := rowMeans(.SD, na.rm=TRUE),
                .SDcols = c("O2Sat", "SaO2")]
-    dat <- dat[, ICULOS := as.difftime(ICULOS - 1, units = "hours")]
-    dat <- as_ts_tbl(dat, "ID")
+    dat <- dat[, stay_time := as.difftime(ICULOS - 1, units = "hours")]
+    dat <- as_ts_tbl(dat, "stay_id", "stay_time")
 
-    sep <- dat[(SepsisLabel), list(ICULOS = min(ICULOS) + 6), by = "ID"]
-    sep <- rename_cols(sep, "sep3_time", "ICULOS")
+    sep <- dat[
+      (SepsisLabel), list(stay_time = min(stay_time) + 6, sepsis_3 = TRUE),
+      by = "stay_id"
+    ]
 
-    dat <- rm_cols(dat, "SepsisLabel")
+    feats <- setNames(feat_map[["concept"]], feat_map[["callenge"]])
+    feats <- feats[data_vars(dat)]
+
+    dat <- rm_cols(dat, c("ICULOS", "SepsisLabel"), by_ref = TRUE)
+    dat <- rename_cols(dat, names(feats), feats)
 
   } else {
 
     pid <- cohort(source)
 
-    dat <- load_dictionary(source, challenge_map[[2L]], id_type = "icustay",
-                           patient_ids = pid)
+    feats <- concepts[["concept"]]
+    feats <- feats[!is.na(feats)]
 
-    dat <- rename_cols(dat, c("ICULOS", challenge_map[[1L]]),
-                            c(index(dat), challenge_map[[2L]]),
-                       skip_absent = TRUE)
+    dat <- load_concepts(feats, source, id_type = "icustay",
+                         patient_ids = pid)
+    dat <- rename_cols(dat, c("stay_id", "stay_time"), meta_vars(dat))
 
     win <- stay_windows(source, id_type = "icustay", win_type = "icustay",
                         in_time = "intime", out_time = "outtime")
 
-    dat  <- dat[, c("join_time") := list(get(index(dat)))]
+    dat  <- dat[, c("join_time") := list(get("stay_time"))]
 
-    join <- c(paste(id(dat), "==", id(win)), "join_time <= outtime")
+    join <- c(paste("stay_id ==", id_vars(win)), "join_time <= outtime")
     dat <- dat[win, on = join]
-    dat <- rm_cols(dat, c("join_time", "intime"))
+    dat <- rm_cols(dat, c("join_time", "intime"), by_ref = TRUE)
 
-    sep <- sepsis3_score(source, pid)
-    sep  <- sep[, c("join_time") := list(get(index(sep)))]
+    sep <- sepsis3_crit(source, pid)
+    sep  <- sep[, c("join_time") := list(get(index_var(sep)))]
 
-    join <- c(paste(id(sep), "==", id(win)), "join_time >= intime",
-                                             "join_time <= outtime")
+    join <- c(paste(id_vars(sep), "==", id_vars(win)), "join_time >= intime",
+                                                       "join_time <= outtime")
     new <- sep[win, on = join]
-    flt <- setdiff(sep[[id(sep)]], new[[id(new)]])
-    sep <- rm_cols(new, data_cols(new))
+    flt <- setdiff(id_col(sep), id_col(new))
+    sep <- rm_cols(new, setdiff(data_vars(new), "sepsis_3"))
 
     if (length(flt) > 0L) {
-      dat <- dat[!get(id(dat)) %in% flt, ]
+      dat <- dat[!get(id_var(dat)) %in% flt, ]
     }
   }
 
-  sep <- data.table::set(sep, j = "SepsisLabel", value = 1L)
+  sep <- sep[, c("sepsis_3") := as.integer(get("sepsis_3"))]
 
   res <- merge(dat, sep, all.x = TRUE)
-  res <- res[, c("SepsisLabel") := data.table::nafill(SepsisLabel, "locf"),
-             by = c(id(res))]
-  res <- res[, c("SepsisLabel") := data.table::nafill(SepsisLabel, fill = 0L)]
+  res <- res[, c("sepsis_3") := data.table::nafill(sepsis_3, "locf"),
+             by = c(id_vars(res))]
+  res <- res[, c("sepsis_3") := data.table::nafill(sepsis_3, fill = 0L)]
 
-  res <- rm_cols(res, setdiff(data_cols(res),
-                              c(challenge_map[[1L]], "SepsisLabel")))
+  res <- res[, c("sepsis_3") := as.logical(get("sepsis_3"))]
 
-  miss_cols <- setdiff(challenge_map[[1L]], data_cols(res))
+  feats <- concepts[["concept"]]
+  feats <- feats[!is.na(feats)]
+
+  res <- rm_cols(res, setdiff(data_vars(res), c(feats, "sepsis_3")))
+
+  miss_cols <- setdiff(feats, data_vars(res))
 
   if (length(miss_cols)) {
     res <- data.table::set(res, j = miss_cols, value = NA_real_)
   }
 
-  res <- data.table::setcolorder(res, c(meta_cols(res), challenge_map[[1L]]))
+  res <- data.table::setcolorder(res, c(meta_vars(res), feats))
 
   dir <- file.path(dir, source)
 
