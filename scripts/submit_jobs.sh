c2b5850976f88ced965a0c21f8d95fc1e1dc3ab7
mi92 <michael.d.moor@gmail.com>
2020-09-10 17:24:17 +0200

    job sub script can use all datasets now that eicu was fixed

--- scripts/submit_jobs.sh
+++ scripts/submit_jobs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 models=(AttentionModel GRUModel)
-# datasets=(PreprocessedPhysionet2019Dataset PreprocessedMIMIC3Dataset PreprocessedEICUDataset PreprocessedHiridDataset)
-datasets=(PreprocessedPhysionet2019Dataset PreprocessedMIMIC3Dataset PreprocessedHiridDataset)
+datasets=(PreprocessedPhysionet2019Dataset PreprocessedMIMIC3Dataset PreprocessedEICUDataset PreprocessedHiridDataset)
 
 res_path=results/hypersearch2
 mkdir -p $res_path
@@ -14,7 +13,7 @@ for dataset in ${datasets[@]};
     do
         out_path=${dataset_path}/${model}
         mkdir -p ${out_path}
-        for rep in {1..5};
+        for rep in {6..20};
             do
             jobname="${model}-${dataset}-${rep}"
             output_log="${out_path}/${rep}.log"
