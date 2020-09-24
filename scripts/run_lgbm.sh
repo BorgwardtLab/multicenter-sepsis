d34ae8c6fae015f709929873f0923aac13f15f1b
mi92 <michael.d.moor@gmail.com>
2020-09-24 17:16:23 +0200

    reduced eicu lgbm jobs in submit script for saving memory

--- scripts/run_lgbm.sh
+++ scripts/run_lgbm.sh
@@ -2,4 +2,4 @@ result_path=$1
 python -m src.sklearn.main --result_path $result_path --cv_n_jobs=20 --n_iter_search=100 --dataset=mimic3
 python -m src.sklearn.main --result_path $result_path --cv_n_jobs=20 --n_iter_search=100 --dataset=physionet2019
 python -m src.sklearn.main --result_path $result_path --cv_n_jobs=20 --n_iter_search=100 --dataset=hirid
-python -m src.sklearn.main --result_path $result_path --cv_n_jobs=10 --n_iter_search=100 --dataset=eicu
+python -m src.sklearn.main --result_path $result_path --cv_n_jobs=5 --n_iter_search=100 --dataset=eicu
