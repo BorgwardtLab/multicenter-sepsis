c477600f151bf998c09dc5ff4d0ada01809ea691
mi92 <michael.d.moor@gmail.com>
2020-10-22 17:02:40 +0200

    updated prepro pipeline bash script

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -4,3 +4,11 @@ python src/sklearn/data/make_dataframe.py --dataset hirid --n_jobs=50 --n_partit
 python src/sklearn/data/make_dataframe.py --dataset physionet2019 --n_jobs=50 --n_partitions=10000 --overwrite
 python src/sklearn/data/make_dataframe.py --dataset eicu --n_jobs=50 --n_partitions=40000 --overwrite
 
+python src/sklearn/data/create_instance_files.py --dataset demo
+python src/sklearn/data/create_instance_files.py --dataset mimic3
+python src/sklearn/data/create_instance_files.py --dataset hirid
+python src/sklearn/data/create_instance_files.py --dataset physionet2019
+python src/sklearn/data/create_instance_files.py --dataset eicu
+
+# Sanity checks: 
+python scripts/check_splits.py
