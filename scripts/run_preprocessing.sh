0e61002e5c3576b34c5f1dfd8df834c106e0dd25
mi92 <michael.d.moor@gmail.com>
2020-10-30 14:42:07 +0100

    updated prepro pipeline bash script to process demo entirely first

--- scripts/run_preprocessing.sh
+++ scripts/run_preprocessing.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+#Demo first:
 python src/sklearn/data/make_dataframe.py --dataset demo --n_jobs=10 --n_partitions=20 --overwrite
+python src/sklearn/data/create_instance_files.py --dataset demo
+
+#Process the full datasets:
 python src/sklearn/data/make_dataframe.py --dataset mimic3 --n_jobs=50 --n_partitions=20000 --overwrite
 python src/sklearn/data/make_dataframe.py --dataset hirid --n_jobs=50 --n_partitions=20000 --overwrite
 python src/sklearn/data/make_dataframe.py --dataset physionet2019 --n_jobs=50 --n_partitions=10000 --overwrite
 python src/sklearn/data/make_dataframe.py --dataset eicu --n_jobs=50 --n_partitions=40000 --overwrite
 
-python src/sklearn/data/create_instance_files.py --dataset demo
 python src/sklearn/data/create_instance_files.py --dataset mimic3
 python src/sklearn/data/create_instance_files.py --dataset hirid
 python src/sklearn/data/create_instance_files.py --dataset physionet2019
