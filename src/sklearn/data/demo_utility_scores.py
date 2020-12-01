1b5aea088ecd68ae02544e17f7bb2e8db522cc11
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-12-01 11:11:40 +0100

    Preparing demo script for utility score calculation
    The goal of this script is to check whether the new utility score
    calculation can be easily integrated into the pipeline. Steps are
    hard-coded currently, as this script has a short half-life.

---
+++ src/sklearn/data/demo_utility_scores.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import pickle
+import sys
+
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+
+from .transformers import *
+from src.sklearn.data.utils import load_pickle, save_pickle
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path',
+        type=str,
+        help='Path from dataset to pickled df file to use as input',
+        default='data/sklearn/processed'
+    )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        help='Dataset to use',
+        default='demo'
+    )
+    parser.add_argument(
+        '--split',
+        type=str,
+        help='Which split to use from [train, validation, test]; if not set '
+             'we loop over all', 
+    )
+
+    args = parser.parse_args()
+    split = args.split 
+    dataset = args.dataset 
+    path = os.path.join('datasets', dataset, args.path)
+     
+    if not split:
+        splits = ['train', 'validation', 'test']
+    else:
+        splits = [split]
+
+    data_pipeline = Pipeline([
+        ('lookback_features', LookbackFeatures()),
+        ('filter_invalid_times', InvalidTimesFiltration()),
+        ('drop_cols', DropColumns(save=False)),
+        ('calculate_utility_scores', CalculateUtilityScores())]
+    )
+
+    print(f'Processing {dataset} and splits {splits}')
+
+    for split in splits: 
+        name = f'X_features_{split}'
+        features_path = os.path.join(path, name + '.pkl')
+        X = load_pickle(features_path)
+
+        data_pipeline.fit_transform(X)
