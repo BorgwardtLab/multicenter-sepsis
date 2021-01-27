95d05edf5c378612033a51ea1c6274ec5aa005c8
mi92 <michael.d.moor@gmail.com>
2021-01-27 11:59:37 +0100

    checked in current sklearn extended sub script

--- scripts/run_sklearn_extended.sh
+++ scripts/run_sklearn_extended.sh
@@ -8,7 +8,8 @@ python -m src.sklearn.main --result_path $result_path --cv_n_jobs=1 --n_iter_sea
 python -m src.sklearn.main --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=aumc --extended_features
 
 #LR
-#python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=mimic3 --extended_features
-#python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=physionet2019 --extended_features
-#python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=hirid --extended_features
-#python -m src.sklearn.main  --method=lr --result_path $result_path --cv_n_jobs=1  --n_iter_search=20 --dataset eicu --extended_features 
+python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=mimic3 --extended_features
+python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=physionet2019 --extended_features --feature_set=challenge
+python -m src.sklearn.main  --method lr --result_path $result_path --cv_n_jobs=1 --n_iter_search=20 --dataset=hirid --extended_features
+python -m src.sklearn.main  --method=lr --result_path $result_path --cv_n_jobs=1  --n_iter_search=20 --dataset eicu --extended_features 
+python -m src.sklearn.main  --method=lr --result_path $result_path --cv_n_jobs=1  --n_iter_search=20 --dataset aumc --extended_features
