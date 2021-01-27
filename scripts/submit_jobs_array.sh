900adbde57fb009ca85f719b6650796dce8b22ed
mi92 <michael.d.moor@gmail.com>
2021-01-27 12:00:07 +0100

    checked in current deep sub script

--- scripts/submit_jobs_array.sh
+++ scripts/submit_jobs_array.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-models=(GRUModel LSTMModel RNNModel) #AttentionModel 
+models=(GRUModel LSTMModel) #(GRUModel) # LSTMModel RNNModel)
+models=(AttentionModel) #AttentionModel 
 # models=(LSTMModel) #AttentionModel 
-# datasets=(PreprocessedMIMIC3Dataset PreprocessedEICUDataset PreprocessedHiridDataset) #PreprocessedPhysionet2019Dataset 
-datasets=(PreprocessedEICUDataset) #PreprocessedPhysionet2019Dataset 
+datasets=(PreprocessedMIMIC3Dataset PreprocessedEICUDataset PreprocessedHiridDataset PreprocessedAUMCDataset) #PreprocessedPhysionet2019Dataset 
+# datasets=(PreprocessedEICUDataset) #PreprocessedPhysionet2019Dataset 
 datasets2=(PreprocessedPhysionet2019Dataset)
+#datasets2=()
 
 res_path=$1
 mkdir -p $res_path
 
 first_index=1
 last_index=19
-parallel_tasks=2
+parallel_tasks=6
 
-for dataset in ${datasets[@]};
+pos_weights=(10 50 50 10) # assuming order: mimic eicu hirid aumc
+pos_weights2=(50) #100
+
+weight_decay=0 #0.001
+
+for index in ${!datasets[*]};
     do
+    dataset=${datasets[$index]}
+    pos_weight=${pos_weights[$index]}
     dataset_path=$res_path/${dataset}
     mkdir -p ${dataset_path}
     for model in ${models[@]};
@@ -26,7 +35,7 @@ for dataset in ${datasets[@]};
             output_log="${out_path}/%a.log"
             output_dir=${out_path}/\$SLURM_ARRAY_TASK_ID
             mkdir -p ${out_path}
-            command="pipenv run train_torch --dataset $dataset --model $model --hyperparam-draws 1 --gpus -1 --log-path $dataset_path --exp-name $model --version \$SLURM_ARRAY_TASK_ID"
+            command="srun pipenv run train_torch --dataset $dataset --model $model --hyperparam-draws 1 --gpus -1 --log-path $dataset_path --exp-name $model --version \$SLURM_ARRAY_TASK_ID --pos-weight $pos_weight --weight-decay $weight_decay"
             sbatch --array $first_index-$last_index%$parallel_tasks -J $jobname --cpus-per-task 2 --mem-per-cpu 8G -n 1 -p gpu -o $output_log -e $output_log --gres=gpu:1 --wrap "if [ ! -d \"$output_dir\" ]; then echo \"$command\"; $command; fi"
         fi
     done
@@ -47,8 +56,8 @@ for dataset in ${datasets2[@]};
             output_log="${out_path}/%a.log"
             output_dir=${out_path}/\$SLURM_ARRAY_TASK_ID
             mkdir -p ${out_path}
-            command="pipenv run train_torch --dataset $dataset --feature-set $feature_set --model $model --hyperparam-draws 1 --gpus -1 --log-path $dataset_path --exp-name $model --version \$SLURM_ARRAY_TASK_ID"
-            sbatch --array $first_index-$last_index%$parallel_tasks -J $jobname --cpus-per-task 2 --mem-per-cpu 5G -n 1 -p gpu -o $output_log -e $output_log --gres=gpu:1 --wrap "if [ ! -d \"$output_dir\" ]; then echo \"$command\"; $command; fi"
+            command="srun pipenv run train_torch --dataset $dataset --feature-set $feature_set --model $model --hyperparam-draws 1 --gpus -1 --log-path $dataset_path --exp-name $model --version \$SLURM_ARRAY_TASK_ID --pos-weight $pos_weights2 --weight-decay $weight_decay"
+            sbatch --array $first_index-$last_index%$parallel_tasks -J $jobname --cpus-per-task 2 --mem-per-cpu 8G -n 1 -p gpu -o $output_log -e $output_log --gres=gpu:1 --wrap "if [ ! -d \"$output_dir\" ]; then echo \"$command\"; $command; fi"
         fi
     done
 done
