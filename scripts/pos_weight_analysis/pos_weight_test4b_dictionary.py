66835964108cefea4dc9da35f767988b4ead70d5
mi92 <michael.d.moor@gmail.com>
2021-02-03 11:50:57 +0100

    checkin in intermediate stage of pos weight test 4b

---
+++ scripts/pos_weight_analysis/pos_weight_test4b_dictionary.py
@@ -0,0 +1,224 @@
+"""
+Here we assess different values of U_FP
+"""
+
+import argparse
+import os
+import pickle
+import sys
+from joblib import Parallel, delayed
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+
+from src.sklearn.data.transformers import *
+from src.sklearn.data.utils import load_pickle, save_pickle, index_check
+from src.evaluation.physionet2019_score import compute_prediction_utility, physionet2019_utility 
+
+import seaborn as sns
+from IPython import embed
+
+def run_task(X, stats, i, n_draws, split, dataset, scorer):
+    """
+    task function for parallelism
+    quadratic heuristic for U_FP
+    """
+    #prev = stats[stats['dataset'] == dataset]['tp_prevalence'].values[0]
+    d = {
+        'hirid': -0.23,
+        'physionet2019': -0.025,
+        'eicu': -0.03,
+        'mimic3': -0.075,
+        'aumc': -0.045,
+    } 
+    u_fp = d[dataset] 
+    utils, util_mu = get_average_util(X, i, n_draws=n_draws, u_fp=u_fp, scorer=scorer) 
+    results = []
+    for j, util in enumerate(utils):
+        result = {}
+        result['p'] = i
+        result['split'] = split 
+        result['dataset'] = dataset
+        result['utility'] = util 
+        result['draw'] = j
+        results.append(result)
+    return results
+
+def get_average_util(X, r, n_draws=10, u_fp=-0.05, scorer=None):
+    """
+    get average util of data X with weight r and n bernoulli draws
+    """
+    preds = np.random.binomial(1,r,(n_draws, len(X)))
+    utils = []
+    labels = X['sep3']
+    for i, pred in enumerate(preds):
+        df = pd.DataFrame(labels)
+        df['pred'] = pred
+        ids = df.index.get_level_values('id').unique()
+        y_true = [df['sep3'].loc[id].values for id in ids]
+        y_pred = [df['pred'].loc[id].values for id in ids]
+        util = scorer(y_true, y_pred) 
+        #util = physionet2019_utility(y_true, y_pred, u_fp=u_fp)
+        #util = compute_prediction_utility(
+        #    labels,
+        #    pred,
+        #    shift_labels=0,
+        #    return_all_scores=False
+        #)
+        utils.append(util)
+    return utils, np.mean(utils)  
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path',
+        type=str,
+        help='Path from dataset to pickled df file to use as input',
+        default='data/sklearn/processed'
+    )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        help='Dataset to use',
+        default='demo'
+    )
+    parser.add_argument(
+        '--split',
+        type=str,
+        help='Which split to use from [train, validation, test]; if not set '
+             'we loop over all', 
+    )
+    parser.add_argument(
+        '--n_jobs',
+        type=int,
+        default=10,
+        help='parallel jobs'
+    )
+
+    args = parser.parse_args()
+    split = args.split 
+    dataset = args.dataset
+    n_draws = 5
+
+    if not split:
+        splits = ['train', 'validation', 'test']
+    else:
+        splits = [split]
+    if dataset == 'all':
+        datasets = ['mimic3', 'physionet2019', 'hirid', 'eicu', 'aumc']
+    else:
+        datasets = [dataset]
+    
+    #results = {}
+    #results['p'] = []
+    #results['split'] = []
+    #results['dataset'] = []
+    #results['r_soft'] = []
+    #results['r_hard'] = []
+    #results['utility'] = []
+    #results['draw'] = []
+    #results['weight'] = []
+    u_fp_dict = {
+        'hirid': -0.23,
+        'physionet2019': -0.025,
+        'eicu': -0.03,
+        'mimic3': -0.075,
+        'aumc': -0.045,
+    } 
+
+    out_path = os.path.join('results', 'pos_weight_analysis')
+    stats = pd.read_csv(
+        os.path.join(out_path, 'dataset_stats.csv')
+    )
+
+    #weights = np.arange(0,1.01,0.1)
+    params =  np.arange(0,1.01,0.05)
+    res_list = [] 
+    for dataset in datasets:
+        
+        path = os.path.join('datasets', dataset, args.path)
+         
+        #data_pipeline = Pipeline([
+        #    ('calculate_utility_scores',
+        #        CalculateUtilityScores(passthrough=False, n_jobs=20))]
+        #)
+
+        u_fp = u_fp_dict[dataset] 
+
+
+        print(f'Processing {dataset} and splits {splits}')
+        for split in splits: 
+            name = f'X_features_{split}'
+            features_path = os.path.join(path, name + '.pkl')
+            X = load_pickle(features_path)
+            X = index_check(X)
+            
+            res = Parallel(n_jobs=args.n_jobs)(
+                delayed(run_task)(  X, 
+                                    stats, 
+                                    i, 
+                                    n_draws, 
+                                    split, 
+                                    dataset ) for i in params)
+            res_list.append(res)
+            #for weight in weights:
+            #         
+            #    u_fp = prev * (-weight) - 0.03 
+            #    # default u_fp was -0.05, we make it weaker to flatten also physionets curve 
+            #    
+            #    # We discard the positive sample weight for now, 
+            #    # TODO: need to update regression target first! 
+            #    """
+            #    X = data_pipeline.fit_transform(X)
+            #    
+            #    u = X['utility']
+            #    s_p = np.sum(u[ u > 0])
+            #    s_n = np.sum(np.abs(u[ u < 0])) # zeros don't matter 
+            #    r_soft = s_p / (s_p + s_n)
+            #    
+            #    s_p = len(u[ u > 0])
+            #    s_n = len(u[ u < 0])    
+            #    r_hard = s_p / (s_p + s_n) 
+            #    print(f'Optimal r_soft = {r_soft}')
+            #    print(f'Optimal r_hard = {r_hard}')
+            #    """
+            #     
+            #    for i in np.arange(0,1.01,0.05):
+            #        utils, util_mu = get_average_util(X, i, n_draws=n_draws, u_fp=u_fp)
+            #        print(f'p = {i:.3f}', f'util = {util_mu:.3f}')
+            #        
+            #        for j, util in enumerate(utils):
+            #            results['p'].append(i)
+            #            results['split'].append(split)
+            #            results['dataset'].append(dataset)
+            #            #results['r_soft'].append(r_soft)
+            #            #results['r_hard'].append(r_hard)
+            #            results['utility'].append(util)
+            #            results['draw'].append(j)
+            #            results['weight'].append(weight)  
+     
+    # unpack parallelized results to dict of lists:
+    results = {}
+    keys = ['p', 'split', 'dataset', 'utility', 'draw']
+    for key in keys:
+        results[key] = [] 
+    
+    for res in res_list: # over datasets and splits
+        for elem in res: # list of parallelized outputs
+            for d in elem: # each output is a list of dicts
+                for key in keys:
+                    results[key].append(d[key])
+    
+    df = pd.DataFrame.from_dict(results)
+    
+    os.makedirs(out_path, exist_ok=True)
+    df.to_csv(
+        os.path.join(out_path, 'random_baselines_parallel_dictionary_u_fp.csv')
+    ) 
+    
+         
+        
+        
