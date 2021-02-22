70b1c658d6d3300b2efa3293c5ea886cddd35b9f
Max Horn <max.horn@bsse.ethz.ch>
2021-02-22 18:39:30 +0100

    Add script to split parquet into row groups

---
+++ src/preprocessing/split_rowgroups.py
@@ -0,0 +1,21 @@
+import argparse
+import pyarrow
+import pyarrow.parquet as pq
+
+
+def main(input_filename, output_filename, chunksize):
+    input_data = pq.read_table(input_filename)
+    index_col = input_data.column_names[0]
+    with pq.ParquetWriter(
+            output_filename, input_data.schema, write_statistics=[index_col]) as f:
+        f.write_table(input_data, row_group_size=chunksize)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_filename', type=str)
+    parser.add_argument('--output', type=str)
+    parser.add_argument('--chunksize', type=int, default=100)
+    args = parser.parse_args()
+
+    main(args.input_filename, args.output, args.chunksize)
