1899817ae42c82ee2a3600e56c021754369f0cc7
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-02-22 18:12:34 +0100

    Simplified normalization script

--- src/preprocessing/normalize.py
+++ src/preprocessing/normalize.py
@@ -6,9 +6,11 @@ import json
 import pathlib
 
 import dask.dataframe as dd
-import numpy as np
+
+from src.sklearn.loading import SplitInfo
 
 from src.preprocessing.transforms import Normalizer
+
 from src.variables.mapping import VariableMapping
 
 
@@ -23,47 +25,14 @@ VM_CONFIG_PATH = str(
 VM_DEFAULT = VariableMapping(VM_CONFIG_PATH)
 
 
-def get_patient_ids_by_split(dataset, split_info, split_name):
-    """Get patient IDs by split name.
-
-    Parameters
-    ----------
-    dataset : str
-        Data set to get split information for, such as 'demo'.
-
-    split_info : dict
-        Information about all potential splits for the given data set.
-        This is typically a JSON file.
-
-    split_name : str
-        Name of split, such as 'dev'.
-
-    Returns
-    -------
-    Array of patient IDs in the desired split.
-    """
-    if split_name == 'dev':
-        ids = split_info[dataset][split_name]['total']
-    else:
-        # TODO: this only provides `dev` data for now.
-        prefix, count = split_name.split('_')
-        ids = split_info[dataset]['dev'][f'split_{count}'][prefix]
-
-    return np.asarray(ids)
-
-
 def main(
     input_filename,
     split_filename,
     output_filename,
     split_name,
-    dataset  # TODO: deprecated parameter
 ):
     """Perform normalization of input data, subject to a certain split."""
-    with open(split_filename) as f:
-        split_info = json.load(f)
-
-    patient_ids = get_patient_ids_by_split(dataset, split_info, split_name)
+    patient_ids = SplitInfo(split_filename)(split_name)
 
     raw_data = dd.read_parquet(
         input_filename,
@@ -80,7 +49,8 @@ def main(
     norm = Normalizer(patient_ids, drop_cols=drop_cols)
     norm = norm.fit(raw_data)
     means, stds = dask.compute(
-        norm.stats['means'], norm.stats['stds']
+        norm.stats['means'],
+        norm.stats['stds']
     )
 
     results = {
@@ -95,8 +65,8 @@ def main(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-file",                     # FIXME
-        default='./dump/features.parquet',  # FIXME
+        "--input-file",
+        required=True,
         type=str,
         help="Path to parquet file or folder with parquet files containing "
              "the raw data.",
@@ -105,16 +75,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--split-file",
         type=str,
-        required=False,
-        default='./dump/master_splits.json',  # FIXME
+        required=True,
         help="JSON file containing split information. Required to ensure "
              "normalization is only computed using the dev split.",
     )
     parser.add_argument(
         '--split-name',
         type=str,
-        required=False,
-        default='dev',
+        required=True,
+        default='train',
+        choices=['train', 'test', 'val', 'dev'],
         help='Indicate split for which the normalization shall be performed.'
     )
 
@@ -123,13 +93,6 @@ if __name__ == "__main__":
         type=str,
         required=True,
         help="Output file path to write parquet file with features.",
-    )
-
-    parser.add_argument(
-        '--dataset',
-        type=str,
-        required=False,
-        default='demo',
     )
 
     args = parser.parse_args()
@@ -142,5 +105,4 @@ if __name__ == "__main__":
         args.split_file,
         args.output_file,
         args.split_name,
-        args.dataset,
     )
