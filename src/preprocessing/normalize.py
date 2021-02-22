648274908dd328e6bf55e78b76f30f905f286e55
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-02-22 12:30:15 +0100

    Updated normalisation script

--- src/preprocessing/normalize.py
+++ src/preprocessing/normalize.py
@@ -2,8 +2,23 @@
 
 import argparse
 import json
+import pathlib
 
+import dask.dataframe as dd
 import numpy as np
+
+from src.variables.mapping import VariableMapping
+
+
+# TODO: use environment variable here?
+VM_CONFIG_PATH = str(
+    pathlib.Path(__file__).parent.parent.parent.joinpath(
+        'config/variables.json'
+    )
+)
+
+VM_DEFAULT = VariableMapping(VM_CONFIG_PATH)
+
 
 # # read patient ids from split files
 # patient_ids = 
@@ -22,27 +37,31 @@ import numpy as np
 # # Write to json file
 
 
-def get_patient_ids_by_split(self, split_info, split_name):
+def get_patient_ids_by_split(dataset, split_info, split_name):
     """Get patient IDs by split name.
 
     Parameters
     ----------
+    dataset : str
+        Data set to get split information for, such as 'demo'.
+
     split_info : dict
-        Information about all potential splits
+        Information about all potential splits for the given data set.
+        This is typically a JSON file.
 
     split_name : str
-        Name of split, such as 'dev'
+        Name of split, such as 'dev'.
 
     Returns
     -------
     Array of patient IDs in the desired split.
     """
     if split_name == 'dev':
-        ids = split_info[split_name]['total']
+        ids = split_info[dataset][split_name]['total']
     else:
         # TODO: this only provides `dev` data for now.
         prefix, count = split_name.split('_')
-        ids = split_info['dev'][f'split_{count}'][prefix]
+        ids = split_info[dataset]['dev'][f'split_{count}'][prefix]
 
     return np.asarray(ids)
 
@@ -56,14 +75,22 @@ def main(input_filename, split_filename, output_filename):
     dataset = 'demo'
     split_name = 'dev'
 
-    print(json.dumps(split_info['demo'], indent=4))
+    patient_ids = get_patient_ids_by_split(dataset, split_info, split_name)
+
+    raw_data = dd.read_parquet(
+        input_filename,
+        # FIXME: missing columns?
+        #columns=VM_DEFAULT.core_set,
+        engine="pyarrow-dataset",
+        chunksize=1,
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--input-file",                         # FIXME
-        default='../../dump/features.parquet',  # FIXME
+        "--input-file",                     # FIXME
+        default='./dump/features.parquet',  # FIXME
         type=str,
         help="Path to parquet file or folder with parquet files containing "
              "the raw data.",
@@ -72,7 +99,7 @@ if __name__ == "__main__":
         "--split-file",
         type=str,
         required=False,
-        default='../../dump/master_splits.json',  # FIXME
+        default='./dump/master_splits.json',  # FIXME
         help="JSON file containing split information. Required to ensure "
              "normalization is only computed using the dev split.",
     )
