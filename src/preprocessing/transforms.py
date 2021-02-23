acf0aaa879fbb590bab4d7f739c56371aef83450
Max Horn <max.horn@bsse.ethz.ch>
2021-02-23 13:41:00 +0100

    Fuse derived feature computation to partition based operation

--- src/preprocessing/transforms.py
+++ src/preprocessing/transforms.py
@@ -218,161 +218,106 @@ class DerivedFeatures(TransformerMixin, BaseEstimator):
 
         # determine variables:
         vm = self.vm
-        temp = vm('temp')
-        hr = vm('hr')
-        resp = vm('resp')
-        pco2 = vm('pco2')
-        wbc = vm('wbc')
+        temp = vm("temp")
+        hr = vm("hr")
+        resp = vm("resp")
+        pco2 = vm("pco2")
+        wbc = vm("wbc")
 
         # Calculate score:
-        temp_data = ((df[temp] > 38) | (df[temp] < 36))
-        temp_data.name = 'temp'
-        hr_data = df[hr] > 90
-        hr_data.name = 'hr'
-        paco2_data = ((df[resp] > 20) | (df[pco2] < 32))
-        paco2_data.name = 'rr.paco2'
-        wbc_data = ((df[wbc] < 4) | (df[wbc] > 12))
-        wbc_data.name = 'wbc_'
+        temp_data = (df[temp] > 38) | (df[temp] < 36) * 1
+        temp_data.name = "temp"
+        hr_data = (df[hr] > 90) * 1
+        hr_data.name = "hr"
+        paco2_data = ((df[resp] > 20) | (df[pco2] < 32)) * 1
+        paco2_data.name = "rr.paco2"
+        wbc_data = ((df[wbc] < 4) | (df[wbc] > 12)) * 1
+        wbc_data.name = "wbc_"
 
-        df_sirs = dd.multi.concat(
-            [temp_data, hr_data, paco2_data, wbc_data], axis=1)
+        # df_sirs = dd.multi.concat([temp_data, hr_data, paco2_data, wbc_data], axis=1)
 
-        # Sum each row, if >= 2 then mar as SIRS
-        sirs = (df_sirs.sum(axis=1) >= 2) * 1.
+        # Sum each row, if >= 2 then mark as SIRS
+        sirs = ((temp_data + hr_data + paco2_data + wbc_data) >= 2) * 1.0
 
         # Leave the binary and the path sirs
-        sirs_df = dd.multi.concat(
-            [sirs, df_sirs.sum(axis=1)], axis=1)
-        sirs_df.columns = ['SIRS', 'SIRS_path']
+        # sirs_df = dd.multi.concat([sirs, df_sirs.sum(axis=1)], axis=1)
+        # sirs_df.columns = ["SIRS", "SIRS_path"]
 
-        return sirs_df
+        return sirs
 
     def mews_score(self, df):
         # mews = np.zeros(shape=df.shape[0])
         # determine variables:
         vm = self.vm
-        sbp = vm('sbp')
-        hr = vm('hr')
-        resp = vm('resp')
-        temp = vm('temp')
+        sbp = vm("sbp")
+        hr = vm("hr")
+        resp = vm("resp")
+        temp = vm("temp")
 
         # sbp
         sbp = df[sbp]
-        # mews[sbp <= 70] += 3
-        # mews[(70 < sbp) & (sbp <= 80)] += 2
-        # mews[(80 < sbp) & (sbp <= 100)] += 1
-        # mews[sbp >= 200] += 2
-        mews = (
-            (sbp <= 70) * 3.
-            + ((70 < sbp) & (sbp <= 80)) * 2.
-            + ((80 < sbp) & (sbp <= 100)) * 1.
-            + (sbp >= 200) * 2.
-        )
-
-        # hr
         hr = df[hr]
-        # mews[hr < 40] += 2
-        # mews[(40 < hr) & (hr <= 50)] += 1
-        # mews[(100 < hr) & (hr <= 110)] += 1
-        # mews[(110 < hr) & (hr < 130)] += 2
-        # mews[hr >= 130] += 3
-        mews += (
-            ((40 < hr) & (hr <= 50)) * 1.
-            + ((100 < hr) & (hr <= 110)) * 1.
-            + ((110 < hr) & (hr < 130)) * 2.
-            + (hr >= 130) * 3.
-        )
-
-        # resp
         resp = df[resp]
-        # mews[resp < 9] += 2
-        # mews[(15 < resp) & (resp <= 20)] += 1
-        # mews[(20 < resp) & (resp < 30)] += 2
-        # mews[resp >= 30] += 3
-        mews += (
-            (resp < 9) * 2.
-            + ((15 < resp) & (resp <= 20)) * 1.
-            + ((20 < resp) & (resp < 30)) * 2.
-            + (resp >= 30) * 3.
-        )
-
-        # temp
         temp = df[temp]
-        # mews[temp < 35] += 2
-        # mews[(temp >= 35) & (temp < 38.5)] += 0
-        # mews[temp >= 38.5] += 2
-        mews += (
-            (temp < 35) * 2.
-            + (temp >= 38.5) * 2.
+        mews = (
+            (sbp <= 70) * 3.0
+            + ((70 < sbp) & (sbp <= 80)) * 2.0
+            + ((80 < sbp) & (sbp <= 100)) * 1.0
+            + (sbp >= 200) * 2.0
+            + ((40 < hr) & (hr <= 50)) * 1.0
+            + ((100 < hr) & (hr <= 110)) * 1.0
+            + ((110 < hr) & (hr < 130)) * 2.0
+            + (hr >= 130) * 3.0
+            + (resp < 9) * 2.0
+            + ((15 < resp) & (resp <= 20)) * 1.0
+            + ((20 < resp) & (resp < 30)) * 2.0
+            + (resp >= 30) * 3.0
+            + (temp < 35) * 2.0
+            + (temp >= 38.5) * 2.0
         )
         return mews
 
     def qSOFA(self, df):
         vm = self.vm
-        resp = vm('resp')
-        sbp = vm('sbp')
+        resp = vm("resp")
+        sbp = vm("sbp")
 
         # qsofa = np.zeros(shape=df.shape[0])
         # qsofa[df[resp].values >= 22] += 1
         # qsofa[df[sbp].values <= 100] += 1
-        return (df[resp] >= 22) * 1. + (df[sbp] <= 100) * 1.
+        return (df[resp] >= 22) * 1.0 + (df[sbp] <= 100) * 1.0
 
     def SOFA(self, df):
         vm = self.vm
-        plt = vm('plt')
-        bili = vm('bili')
-        map = vm('map')
-        crea = vm('crea')
+        plt = vm("plt")
+        bili = vm("bili")
+        map = vm("map")
+        crea = vm("crea")
 
         # sofa = np.zeros(shape=df.shape[0])
 
-        # Coagulation
         platelets = df[plt]
-        # sofa[platelets >= 150] += 0
-        # sofa[(100 <= platelets) & (platelets < 150)] += 1
-        # sofa[(50 <= platelets) & (platelets < 100)] += 2
-        # sofa[(20 <= platelets) & (platelets < 50)] += 3
-        # sofa[platelets < 20] += 4
-        sofa = (
-            ((100 <= platelets) & (platelets < 150)) * 1.
-            + ((50 <= platelets) & (platelets < 100)) * 2.
-            + ((20 <= platelets) & (platelets < 50)) * 3.
-            + (platelets < 20) * 4.
-        )
-
-        # Liver
         bilirubin = df[bili]
-        # sofa[bilirubin < 1.2] += 0
-        # sofa[(1.2 <= bilirubin) & (bilirubin <= 1.9)] += 1
-        # sofa[(1.9 < bilirubin) & (bilirubin <= 5.9)] += 2
-        # sofa[(5.9 < bilirubin) & (bilirubin <= 11.9)] += 3
-        # sofa[bilirubin > 11.9] += 4
-
-        sofa += (
-            ((1.2 <= bilirubin) & (bilirubin <= 1.9)) * 1.
-            + ((1.9 < bilirubin) & (bilirubin <= 5.9)) * 2.
-            + ((5.9 < bilirubin) & (bilirubin <= 11.9)) * 3.
-            + (bilirubin > 11.9) * 4.
-        )
-
-        # Cardiovascular
         map = df[map]
-        # sofa[map >= 70] += 0
-        # sofa[map < 70] += 1
-        sofa += (map < 70) * 1.
-
-        # crea
         creatinine = df[crea]
-        # sofa[creatinine < 1.2] += 0
-        # sofa[(1.2 <= creatinine) & (creatinine <= 1.9)] += 1
-        # sofa[(1.9 < creatinine) & (creatinine <= 3.4)] += 2
-        # sofa[(3.4 < creatinine) & (creatinine <= 4.9)] += 3
-        # sofa[creatinine > 4.9] += 4
-        sofa += (
-            ((1.2 <= creatinine) & (creatinine <= 1.9)) * 1.
-            + ((1.9 < creatinine) & (creatinine <= 3.4)) * 2.
-            + ((3.4 < creatinine) & (creatinine <= 4.9)) * 3.
-            + (creatinine > 4.9) * 4.
+        sofa = (
+            # Coagulation
+            ((100 <= platelets) & (platelets < 150)) * 1.0
+            + ((50 <= platelets) & (platelets < 100)) * 2.0
+            + ((20 <= platelets) & (platelets < 50)) * 3.0
+            + (platelets < 20) * 4.0
+            # Liver
+            + ((1.2 <= bilirubin) & (bilirubin <= 1.9)) * 1.0
+            + ((1.9 < bilirubin) & (bilirubin <= 5.9)) * 2.0
+            + ((5.9 < bilirubin) & (bilirubin <= 11.9)) * 3.0
+            + (bilirubin > 11.9) * 4.0
+            # Cardiovascular
+            + (map < 70) * 1.0
+            # crea
+            + ((1.2 <= creatinine) & (creatinine <= 1.9)) * 1.0
+            + ((1.9 < creatinine) & (creatinine <= 3.4)) * 2.0
+            + ((3.4 < creatinine) & (creatinine <= 4.9)) * 3.0
+            + (creatinine > 4.9) * 4.0
         )
 
         return sofa
@@ -380,55 +325,64 @@ class DerivedFeatures(TransformerMixin, BaseEstimator):
     def SOFA_deterioration(self, s):
         def check_24hr_deterioration(s):
             """ Check the max deterioration over the last 24 hours, if >= 2 then mark as a 1"""
-            min_prev_23_hrs = s.rolling(24, min_periods=0).apply(np.nanmin)
+            min_prev_23_hrs = s.rolling(24, min_periods=0).apply(np.nanmin, raw=True)
             return s - min_prev_23_hrs
-        sofa_det = s.groupby(
-            s.index.name,
-            sort=False).apply(check_24hr_deterioration, meta=('SOFA_derived', 'f8'))
+
+        sofa_det = s.groupby(s.index.name, sort=False).apply(check_24hr_deterioration)
         # Remove negative sofa values
-        sofa_det = (sofa_det < 0) * 0. + (sofa_det >= 0) * sofa_det
+        sofa_det = (sofa_det < 0) * 0.0 + (sofa_det >= 0) * sofa_det
         return sofa_det
 
     def septic_shock(self, df):
         vm = self.vm
-        map = vm('map')
-        lact = vm('lact')
+        map = vm("map")
+        lact = vm("lact")
 
         # shock = np.zeros(shape=df.shape[0])
         # shock[df[map].values < 65] += 1
         # shock[df[lact].values > 2] += 1
         return (df[map] < 65) * 1 + (df[lact] > 2) * 1
 
-    def transform(self, df):
+    def transform(self, ddf):
         vm = self.vm
-        hr = vm('hr')
-        sbp = vm('sbp')
-        bun = vm('bun')
-        crea = vm('crea')
-        po2 = vm('po2')
-        fio2 = vm('fio2')
-        plt = vm('plt')
-        map = vm('map')
-        bili = vm('bili')
 
-        # Ratios:
-        df['ShockIndex_derived'] = df[hr] / df[sbp]
-        df['bun/cr_derived'] = df[bun] / df[crea]
-        df['po2/fio2_dervied'] = df[po2] / \
-            df[fio2]  # shouldnt it be PaO2/Fi ratio?
+        def partition_wise_function(df):
+            """Function to apply to each partition.
 
-        # SOFA
-        df['SOFA_derived'] = self.SOFA(df[[plt, map, crea, bili]])
-        df['SOFA_deterioration_derived'] = self.SOFA_deterioration(
-            df['SOFA_derived'])
-        df['qSOFA_derived'] = self.qSOFA(df)
-        df['SepticShock_derived'] = self.septic_shock(df)
+            This reduces the number of dask tasks that need to be created
+            significantly.
+            """
+            hr = vm("hr")
+            sbp = vm("sbp")
+            bun = vm("bun")
+            crea = vm("crea")
+            po2 = vm("po2")
+            fio2 = vm("fio2")
+            plt = vm("plt")
+            map = vm("map")
+            bili = vm("bili")
 
-        # Other scores
-        sirs_df = self.sirs_criteria(df)
-        df['MEWS_derived'] = self.mews_score(df)
-        df['SIRS_derived'] = sirs_df['SIRS']
-        return df
+            sofa = self.SOFA(df[[plt, map, crea, bili]])
+
+            return df.assign(
+                **{
+                    # Ratios:
+                    "ShockIndex_derived": df[hr] / df[sbp],
+                    "bun/cr_derived": df[bun] / df[crea],
+                    "po2/fio2_dervied": df[po2]
+                    / df[fio2],  # shouldnt it be PaO2/Fi ratio?
+                    # SOFA
+                    "SOFA_derived": sofa,
+                    "SOFA_deterioration_derived": self.SOFA_deterioration(sofa),
+                    "qSOFA_derived": self.qSOFA(df),
+                    "SepticShock_derived": self.septic_shock(df),
+                    # Other scores
+                    "MEWS_derived": self.mews_score(df),
+                    "SIRS_derived": self.sirs_criteria(df),
+                }
+            )
+
+        return ddf.map_partitions(partition_wise_function)
 
 
 class Normalizer(TransformerMixin, BaseEstimator):
