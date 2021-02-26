1185c6130b8b86b0c91ef33aa2f5a2a68001aa10
mi92 <michael.d.moor@gmail.com>
2021-02-26 17:34:45 +0100

    updated lambda calc with argparse for split, input file etc (similar to normalize) and moved to preprocessing

--- src/preprocessing/lambda.py
+++ src/preprocessing/lambda.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pandas as pd 
 from joblib import Parallel, delayed
+import argparse
+import pathlib
+import json
 
 from IPython import embed
 from src.variables.mapping import VariableMapping
 from src.sklearn.data.utils import load_pickle #save_pickle, index_check
-from .physionet2019_score import compute_prediction_utility 
-from .sklearn_utils import make_consecutive
+from src.evaluation.physionet2019_score import compute_prediction_utility 
+from src.evaluation.sklearn_utils import make_consecutive
+
+VM_CONFIG_PATH = str(
+    pathlib.Path(__file__).parent.parent.parent.joinpath(
+        'config/variables.json'
+    )
+)
+
+VM_DEFAULT = VariableMapping(VM_CONFIG_PATH)
 
 class LambdaCalculator:
     """
@@ -14,21 +25,19 @@ class LambdaCalculator:
     prevalence correction of clinical utility score.
 
     """
-    def __init__(self, n_jobs=10, n_chunks=50, u_fp=-0.05, early_window=12, vm=None, shift=-6):
+    def __init__(self, n_jobs=10, n_chunks=50, u_fp=-0.05, early_window=12, shift=-6):
         """
         Inputs: 
             - n_jobs: number of jobs for parallelism
             - u_fp: U_{FP} value as used in util score
             - early_window: duration from t_early to t_sepsis 
                 in util score
-            - vm: Variable Mapping obj
             - shift: label shift, default: -6 for 6 hours into the past
         """
         self.n_jobs = n_jobs
         self.n_chunks = n_chunks
         self.u_fp = u_fp
         self.early_window = early_window
-        self.vm = vm 
         self.shift = shift
          
     def _process_chunk_of_patients(self, data):
@@ -42,9 +51,9 @@ class LambdaCalculator:
         ids = data.index.get_level_values(0).unique()
         for i in ids:
             df = data.loc[[i]] 
-            df = df.reset_index().set_index(self.vm('time')) 
+            df = df.reset_index().set_index(VM_DEFAULT('time')) 
  
-            y = df[self.vm('label')]
+            y = df[VM_DEFAULT('label')]
 
             # we assume that there are no remaining NaNs! assert this
             n_nans = df.isnull().sum().sum()
@@ -191,17 +200,56 @@ class SimplifiedLambdaCalculator:
         return lam
 
 if __name__ == "__main__":
-    result = {}
-    datasets = ['demo']
-    path = 'datasets/{}/data/parquet/features.parquet'
-    vm = VariableMapping()
-    for dataset in datasets:
-        X = pd.read_parquet(path.format(dataset), columns=['sep3', 'stay_time'])
-        calc = LambdaCalculator(n_jobs=1, vm=vm)
-        lam = calc(X)
-        result[dataset] = lam
-    print(result)
-    with open('config/lambdas.json', 'w') as f:
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-file",
+        required=True,
+        type=str,
+        help="Path to parquet file or folder with parquet files containing "
+             "the raw data."
+    )
+    parser.add_argument(
+        "--split-file",
+        type=str,
+        required=True,
+        help="JSON file containing split information. Required to ensure "
+             "lambda is only computed using the correct training split."
+    )
+    parser.add_argument(
+        '--split-name',
+        type=str,
+        required=True,
+        default='train',
+        choices=['train', 'test', 'val', 'dev'],
+        help='Indicate split for which lambda should be calculated.'
+    )
+    parser.add_argument(
+        '-r', '--repetition',
+        type=int,
+        default=0,
+        help='Repetition of split to load'
+    )
+    parser.add_argument(
+        "--output-file",
+        type=str,
+        required=True,
+        help="Output file path."
+    )
+    parser.add_argument(
+        "--n-jobs",
+        type=int,
+        default=10,
+        help="number of jobs for parallelizing the calculation."
+    )
+
+    args = parser.parse_args()
+    
+    X = pd.read_parquet(args.input_file, columns=['sep3', 'stay_time'])
+    calc = LambdaCalculator(n_jobs=args.n_jobs)
+    lam = calc(X)
+    results = {'lam': lam}
+    with open(args.output_file, 'w') as f:
         json.dump(results, f)
 
 
