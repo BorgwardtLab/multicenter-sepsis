d118c8d9d9d48d440b8da840a8be46a35996a322
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-03-02 16:07:36 +0100

    merge physionet and ricu preproc

--- r/utils/ricu-load.R
+++ r/utils/ricu-load.R
@@ -1,9 +1,10 @@
 
-load_physionet <- function(dir = data_path("physionet2019"),
-                           cfg = cfg_path("variables.json")) {
+load_physionet <- function(var_cfg = cfg_path("variables.json"),
+                           coh_cfg = cfg_path("cohorts.json"),
+                           data_dir = data_path("physionet2019")) {
 
-  concepts <- read_var_json(cfg)
-  data_dir <- file.path(dir, "training_setB")
+  concepts <- read_var_json(var_cfg)
+  data_dir <- file.path(data_dir, "training_setB")
 
   assert(dir.exists(data_dir), msg = "Directory {data_dir} not found.")
 
@@ -30,128 +31,23 @@ load_physionet <- function(dir = data_path("physionet2019"),
   feats <- setNames(feat_map[["concept"]], feat_map[["callenge"]])
   feats <- feats[data_vars(dat)]
 
+  win <- collapse(dat, start = "intime", end = "outtime")
   dat <- rename_cols(dat, feats, names(feats), by_ref = TRUE)
 
-  dat <- merge(dat, sep, all = TRUE)
+  res <- unmerge(dat)
+  names(res) <- vapply(res, data_vars, character(1L))
 
-  dat <- dat[, all_miss := Reduce(`&`, lapply(.SD, is.na)),
-             .SDcols = data_vars(dat)]
+  cnc <- concepts$concept[!is.na(concepts$concept)]
+  mis <- !cnc %in% names(res)
 
-  msg("--> loading complete")
-
-  dat
-}
-
-load_ricu <- function(source, var_cfg = cfg_path("variables.json"),
-                      coh_cfg = cfg_path("cohorts.json"),
-                      min_stay_length = hours(6L), min_n_meas = 4L,
-                      min_onset = hours(4L), max_onset = days(7L),
-                      cut_case = hours(24L), cut_ctrl = max_onset + cut_case) {
-
-  pmin_dt <- function(x, y) {
-    x[x > y] <- `units<-`(y, units(x))
-    x
+  if (any(mis)) {
+    add <- res[[1L]][0L, ]
+    add <- Map(rename_cols, list(add), cnc[mis], data_var(add))
+    names(add) <- cnc[mis]
+    res <- c(res, add)
   }
 
-  truncate_dat <- function(dat, win) {
-
-    join <- paste(id_var(dat), "==", id_var(win))
-    vars <- c(meta_vars(dat), data_var(dat))
-
-    if (is_ts_tbl(dat)) {
-      dat  <- dat[, c("join_time") := list(get(index_var(dat)))]
-      join <- c(join, "join_time <= cuttime")
-    }
-
-    dat <- dat[win, on = join, nomatch = NULL]
-    dat <- rm_cols(dat, setdiff(colnames(dat), vars))
-
-    dat
-  }
-
-  feats <- read_var_json(var_cfg)[["concept"]]
-  feats <- feats[!is.na(feats)]
-  cohor <- jsonlite::read_json(coh_cfg, simplifyVector = TRUE, flatten = TRUE)
-  pids  <- unlist(cohor[[source]]$initial)
-
-  win <- stay_windows(source, id_type = "icustay", win_type = "icustay",
-                      in_time = "intime", out_time = "outtime",
-                      patient_ids = pids)
-
-  dat <- load_concepts(feats, source, merge_data = FALSE,
-                       id_type = "icustay", patient_ids = pids)
-  sep <- sepsis3_crit(source, pids, dat)
-
-  tmp <- rm_cols(sep, data_vars(sep))
-  tmp <- rename_cols(tmp, "sep_time", index_var(sep), by_ref = TRUE)
-
-  win <- merge(win, tmp, all.x = TRUE)
-  tmp <- nrow(win)
-
-  win <- win[is.na(sep_time) | (sep_time >= intime & sep_time <= outtime), ]
-
-  msg("--> removing {tmp - nrow(win)} patients due to onsets",
-      " outside of icu stay.\n")
-
-  tmp <- nrow(win)
-  win <- win[is.na(sep_time) | (sep_time >= min_onset &
-                                sep_time <= max_onset), ]
-
-  msg("--> removing {tmp - nrow(win)} patients due to onsets",
-      " outside of [{format_unit(min_onset)},",
-      " {format_unit(max_onset)}].\n")
-
-  tmp <- nrow(win)
-  win <- win[outtime > min_stay_length, ]
-
-  msg("--> removing {tmp - nrow(win)} patients due to stay length <",
-      " {format_unit(min_stay_length)}].\n")
-
-  win <- win[, cuttime := data.table::fifelse(
-    is.na(sep_time), pmin_dt(outtime, cut_ctrl), sep_time + cut_case
-  )]
-
-  msg("--> removing up to {format_unit(sum(win$outtime - win$cuttime))}",
-      " due to censoring data {format_unit(cut_case)} after onsets",
-      " and {format_unit(cut_ctrl)} into stays.\n")
-
-  dat <- lapply(c(dat, list(sep)), truncate_dat, win)
-
-  is_ts <- vapply(dat, is_ts_tbl, logical(1L))
-  is_id <- vapply(dat, is_id_tbl, logical(1L)) & ! is_ts
-
-  dat <- dat[c(which(is_ts), which(is_id))]
-
-  while(length(dat) > 1L) {
-    dat[[1L]] <- merge(dat[[1L]], dat[[2L]], all = TRUE)
-    dat[[2L]] <- NULL
-  }
-
-  dat <- dat[[1L]]
-  dat <- rename_cols(dat, c("stay_id", "stay_time"), meta_vars(dat),
-                     by_ref = TRUE)
-
-  dat <- rm_na(dat, meta_vars(dat), "any")
-
-  cnt <- dat[stay_time > 0, .N, by = c("stay_id")]
-  cnt <- cnt[N >= min_n_meas, ]
-  cnt <- cnt[, N := NULL]
-
-  msg("--> removing {length(unique(dat[['stay_id']])) - nrow(cnt)}",
-      " patients due to fewer than {min_n_meas} in-icu measurements")
-
-  dat <- merge(dat, cnt, all.y = TRUE)
-
-  cohor[[source]]$final <- unique(id_col(dat))
-  jsonlite::write_json(cohor, coh_cfg, pretty = TRUE)
-
-  dat <- dat[, all_miss := FALSE]
-  dat <- fill_gaps(dat)
-  dat <- dat[, all_miss := is.na(all_miss)]
-
-  msg("--> loading complete")
-
-  dat
+  list(win = win, dat = res[cnc], sep = sep)
 }
 
 sepsis3_crit <- function(source, pids = NULL,
@@ -184,6 +80,172 @@ sepsis3_crit <- function(source, pids = NULL,
   sep3(dat, si, si_window = "any")
 }
 
+load_ricu <- function(source, var_cfg = cfg_path("variables.json"),
+                      coh_cfg = cfg_path("cohorts.json")) {
+
+  feats <- read_var_json(var_cfg)[["concept"]]
+  feats <- feats[!is.na(feats)]
+  cohor <- jsonlite::read_json(coh_cfg, simplifyVector = TRUE, flatten = TRUE)
+  pids  <- unlist(cohor[[source]]$initial)
+
+  win <- stay_windows(source, id_type = "icustay", win_type = "icustay",
+                      in_time = "intime", out_time = "outtime",
+                      patient_ids = pids)
+
+  dat <- load_concepts(feats, source, merge_data = FALSE,
+                       id_type = "icustay", patient_ids = pids)
+  sep <- sepsis3_crit(source, pids, dat)
+
+  list(win = win, dat = dat, sep = sep)
+}
+
+load_data <- function(source, var_cfg = cfg_path("variables.json"), ...,
+                      min_stay_len = hours(6L), min_n_meas = 4L,
+                      min_onset = hours(4L), max_onset = days(7L),
+                      cut_case = hours(24L), cut_ctrl = max_onset + cut_case) {
+
+  pmin_dt <- function(x, y) {
+    x[x > y] <- `units<-`(y, units(x))
+    x
+  }
+
+  truncate_dat <- function(dat, win) {
+
+    join <- paste(id_var(dat), "==", id_var(win))
+    vars <- c(meta_vars(dat), data_var(dat))
+
+    if (is_ts_tbl(dat)) {
+      dat  <- dat[, c("join_time") := list(get(index_var(dat)))]
+      join <- c(join, "join_time <= cuttime")
+    }
+
+    dat <- dat[win, on = join, nomatch = NULL]
+    dat <- rm_cols(dat, setdiff(colnames(dat), vars))
+
+    dat
+  }
+
+  assert(is.string(source))
+
+  dat <- prof(
+    if (identical(source, "physionet2019")) {
+      load_physionet(var_cfg = var_cfg, ...)
+    } else {
+      load_ricu(source, var_cfg = var_cfg, ...)
+    }
+  )
+
+  nav <- vapply(dat$dat, nrow, integer(1L)) == 0
+
+  msg("--> successfully loaded {length(dat$dat)} features, {sum(nav)}",
+      " of which are fully missing")
+
+  tmp <- rm_cols(dat$sep, data_vars(dat$sep))
+  tmp <- rename_cols(tmp, "sep_time", index_var(dat$sep), by_ref = TRUE)
+
+  win <- merge(dat$win, tmp, all.x = TRUE)
+  tmp <- nrow(win)
+
+  win <- win[is.na(sep_time) | (sep_time >= intime & sep_time <= outtime), ]
+
+  msg("--> removing {tmp - nrow(win)} patients due to onsets",
+      " outside of icu stay.\n")
+
+  tmp <- nrow(win)
+  win <- win[is.na(sep_time) | (sep_time >= min_onset &
+                                sep_time <= max_onset), ]
+
+  msg("--> removing {tmp - nrow(win)} patients due to onsets",
+      " outside of [{format_unit(min_onset)},",
+      " {format_unit(max_onset)}].\n")
+
+  tmp <- nrow(win)
+  win <- win[outtime > min_stay_len, ]
+
+  msg("--> removing {tmp - nrow(win)} patients due to stay length <",
+      " {format_unit(min_stay_len)}].\n")
+
+  win <- win[, cuttime := data.table::fifelse(
+    is.na(sep_time), pmin_dt(outtime, cut_ctrl), sep_time + cut_case
+  )]
+
+  msg("--> removing up to {format_unit(sum(win$outtime - win$cuttime))}",
+      " due to censoring data {format_unit(cut_case)} after onsets",
+      " and {format_unit(cut_ctrl)} into stays.\n")
+
+  dat <- lapply(c(dat$dat, list(dat$sep)), truncate_dat, win)
+
+  is_ts <- vapply(dat, is_ts_tbl, logical(1L))
+  is_id <- vapply(dat, is_id_tbl, logical(1L)) & ! is_ts
+
+  sta <- dat[which(is_id)]
+  dat <- dat[which(is_ts)]
+
+  while(length(dat) > 1L) {
+    dat[[1L]] <- merge(dat[[1L]], dat[[2L]], all = TRUE)
+    dat[[2L]] <- NULL
+  }
+
+  tmp <- nrow(dat[[1L]])
+
+  dat <- dat[[1L]][, all_miss := FALSE]
+  dat <- fill_gaps(dat)
+  dat <- dat[, all_miss := is.na(all_miss)]
+
+  msg("--> adding {nrow(dat) - tmp} time points to create a regular time",
+      " series.\n")
+
+  if (length(sta) > 0L) {
+
+    while(length(sta) > 1L) {
+      sta[[1L]] <- merge(sta[[1L]], sta[[2L]], all = TRUE)
+      sta[[2L]] <- NULL
+    }
+
+    dat <- merge(dat, sta[[1L]], all = TRUE)
+  }
+
+  dat <- rm_na(dat, meta_vars(dat), "any")
+  dat <- rename_cols(dat, c("stay_id", "stay_time"), meta_vars(dat),
+                     by_ref = TRUE)
+
+  cfg <- read_var_json(var_cfg)
+  cfg <- cfg[!is.na(cfg$concept), ]
+
+  tsn <- cfg[!cfg$category %in% c("static", "baseline"), ]
+  exp <- !nav[tsn$concept]
+  exp <- names(exp[exp])
+
+  dat <- dat[, ts_miss := as.integer(
+    rowSums(data.table::setDF(lapply(.SD, is.na)))
+  ), .SDcols = exp]
+  dat <- dat[, ts_avail := ts_miss < length(exp)]
+
+  cnt <- dat[stay_time >= 0, list(ts_count = sum(ts_avail)), by = c("stay_id")]
+  cnt <- cnt[ts_count >= min_n_meas, ]
+  cnt <- cnt[, ts_count := NULL]
+
+  tmp <- nrow(dat)
+  dat <- merge(dat, cnt, all.y = TRUE)
+
+  msg("--> removing {nrow(tmp) - nrow(dat)} patients due to fewer",
+      "  than {min_n_meas} in-icu measurements")
+
+  dat <- rename_cols(dat, cfg$name, cfg$concept, by_ref = TRUE,
+                     skip_absent = TRUE)
+
+  stopifnot(all(cfg$name %in% data_vars(dat)))
+
+  exp <- c(meta_vars(dat), cfg$name)
+  dat <- data.table::setcolorder(dat, c(exp, setdiff(colnames(dat), exp)))
+
+  dat <- rename_cols(dat, paste0(tsn$name, "_raw"), tsn$name, by_ref = TRUE)
+
+  msg("--> loading complete")
+
+  dat
+}
+
 augment <- function(x, fun, suffix,
                     cols = grep("_raw$", colnames(x), value = TRUE),
                     by = NULL, win = NULL, tmpdir = tempdir(),
@@ -204,54 +266,6 @@ augment <- function(x, fun, suffix,
   } else if (is.null(win)) {
 
     res <- x[, lapply(.SD, fun, ...), .SDcols = c(cols), by = c(by)]
-
-  } else if (is.character(fun)) {
-
-    fix_inf <- FALSE
-
-    id_cols <- id_vars(x)
-    ind_col <- index_var(x)
-
-    tmp_col <- c("tmp_ind_1", "tmp_ind_2")
-
-    wins <- x[,
-      c(mget(id_cols), list(min_time = get(ind_col) - win,
-                            max_time = get(ind_col)))
-    ]
-
-    x <- x[, c(tmp_col) := list(get(ind_col), get(ind_col))]
-    on.exit(rm_cols(x, tmp_col, by_ref = TRUE))
-
-    join <- paste(c(id_cols, tmp_col),
-                  c(rep("==", length(id_cols)), "<=", ">="),
-                  c(id_cols, "max_time", "min_time"))
-
-    res <- withCallingHandlers({
-      switch(fun,
-        min  = x[wins, lapply(.SD, min,  ...), .SDcols = cols, on = join,
-                 by = .EACHI],
-        max  = x[wins, lapply(.SD, max,  ...), .SDcols = cols, on = join,
-                 by = .EACHI],
-        mean = x[wins, lapply(.SD, mean, ...), .SDcols = cols, on = join,
-                 by = .EACHI],
-        var  = x[wins, lapply(.SD, var,  ...), .SDcols = cols, on = join,
-                 by = .EACHI]
-      )
-    }, warning = function(w) {
-
-      if (identical(substr(conditionMessage(w), 1, 24),
-                    "no non-missing arguments")) {
-
-        fix_inf <<- TRUE
-        invokeRestart("muffleWarning")
-      }
-    })
-
-    res <- rm_cols(res, tmp_col, by_ref = TRUE)
-
-    if (fix_inf) {
-      res <- res[, c(cols) := lapply(.SD, inf_to_na), .SDcols = c(cols)]
-    }
 
   } else {
 
@@ -284,52 +298,28 @@ prof <- function(expr, envir = parent.frame()) {
 }
 
 export_data <- function(src, dest_dir = data_path("export"),
-                        var_cfg = cfg_path("variables.json"), ...) {
+                        coh_cfg = cfg_path("cohorts.json"), ...) {
 
-  aug_fun_win <- function(fun, win, suf)
+  augment_prof <- function(...) prof(augment(...))
 
   assert(is.string(src), dir.exists(dir))
 
-  dat <- prof(
-    if (identical(src, "physionet2019")) {
-      load_physionet(cfg = var_cfg)
-    } else {
-      load_ricu(src, var_cfg = var_cfg, ...)
-    }
-  )
+  dat <- load_data(src, coh_cfg = coh_cfg, ...)
+
+  cohor <- jsonlite::read_json(coh_cfg, simplifyVector = TRUE, flatten = TRUE)
+  cohor[[src]]$final <- unique(id_col(dat))
+  jsonlite::write_json(cohor, coh_cfg, pretty = TRUE)
+
+  dat <- dat[, c("female") := list(female == "Female")]
 
   dat <- dat[, onset := sep3]
   dat <- replace_na(dat, type = "locf", by_ref = TRUE, vars = "sep3",
                     by = id_vars(dat))
   dat <- replace_na(dat, FALSE, by_ref = TRUE, vars = "sep3")
-  dat <- dat[, c("sex") := list(sex == "Female")]
 
-  cfg <- read_var_json(var_cfg)
-  cfg <- cfg[!is.na(cfg$concept), ]
-
-  dat <- rename_cols(dat, cfg$name, cfg$concept, by_ref = TRUE,
-                     skip_absent = TRUE)
-
-  missing <- !cfg$name %in% data_vars(dat)
-
-  if (any(missing)) {
-    dat <- dat[, c(cfg$name[missing]) := NA_real_]
-  }
-
-  dat <- data.table::setcolorder(dat, c(meta_vars(dat), cfg$name))
-
-  tsn <- cfg$name[!cfg$category %in% c("static", "baseline")]
-  tsv <- paste0(tsn, "_raw")
-  dat <- rename_cols(dat, tsv, tsn, by_ref = TRUE)
-
-  ind <- prof(
-    augment(dat, Negate(is.na), "ind", tsv)
-  )
-
-  lof <- prof(
-    augment(dat, data.table::nafill, "locf", tsv, by = id_vars(dat),
-            type = "locf")
-  )
+  ind <- augment_prof(dat, Negate(is.na), "ind")
+  lof <- augment_prof(dat, data.table::nafill, "locf", by = id_vars(dat),
+                      type = "locf")
 
   dat <- dat[, c(index_var(dat)) := as.double(
     get(index_var(dat)), units = "hours"
