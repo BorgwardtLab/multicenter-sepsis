4261d6f96a3e7feaa89aca604ebc62ec9dede5db
Drago Plecko <drago.plecko@stat.math.ethz.ch>
2021-03-04 21:14:48 +0100

    Patient table completed

--- r/utils/pts-tbl-helpers.R
+++ r/utils/pts-tbl-helpers.R
@@ -18,15 +18,33 @@ srcwrap <- function(src) {
   }
 }
 
-undemo <- function(src) {
-
-  if(src == "mimic_demo") return("mimic")
-  if(src == "eicu_demo") return("eicu")
-
-  return(src)
-
+clean_unit_str <- function(x) {
+  x <- gsub("/l", "/L", x)
+  x <- gsub("/dl", "/dL", x)
+  x <- gsub("mmhg", "mmHg", x)
+  x <- gsub("meq", "mEq", x)
+  x <- gsub("\\(iu", "\\(IU", x)
+  x
 }
 
+nnquant <- function(x, y, upto = hours(24L)) {
+
+  x <- x[get(index_var(x)) <= upto]
+  val_col <- setdiff(names(x), meta_vars(x))
+  x <- x[[val_col]]
+
+  med <- round(median(x, na.rm = T), 2)
+  IQR <- quantile(x, c(0.25, 0.75), na.rm = T)
+  n <- length(x)
+
+  res <- paste0(med, " (", round(IQR[1], 2), "-", round(IQR[2], 2), ")")
+
+  c(
+    "Number,\n Number per patient stay,\n Median (IQR)",
+    paste(n, round(sum(!is.na(x))/length(y), 2), res, sep = ",\n")
+  )
+
+}
 
 med_iqr <- function(x, patient_ids) {
   val_col <- setdiff(names(x), meta_vars(x))
@@ -79,12 +97,25 @@ percent_fun <- function(x, patient_ids) {
 
 }
 
+count_percent <- function(x, patient_ids) {
+
+  val_col <- setdiff(names(x), meta_vars(x))
+  list(val_col,
+    "n (%)",
+    paste0(
+      sum(x[[val_col]]), " (",
+      round(100 * sum(x[[val_col]]) / length(patient_ids)), ")"
+    )
+  )
+
+}
+
 concept_translator <- list(
   age = "Age (years)",
   med = "- Medical",
   other = "- Other",
   surg = "- Surgical",
-  death = "Mortality",
+  death = "In-hospital mortality",
   `Cohort size` = "Cohort size",
   los_icu = "ICU LOS",
   los_hosp = "Hospital LOS (days)",
@@ -97,28 +128,13 @@ concept_translator <- list(
   sofa_liver_comp = "- Hepatic",
   sofa_cardio_comp = "- Cardiovascular",
   sofa_renal_comp = "- Renal",
-  sep3 = "Sepsis-3 prevalence"
+  is_vaso = "Patients on vasopressors",
+  is_abx = "Patients on antibiotics",
+  is_vent = "Ventilated patients",
+  is_si = "Patients with SI",
+  DM = "Diabetes",
+  LD = "Liver Disease",
+  CPD = "Chronic Pulmonary Disease",
+  CRF = "Chronic Renal Failure",
+  Cancer = "Cancer"
 )
-
-s3_pts_tbl <- function(source, pids = NULL) {
-
-  if (grepl("eicu", source)) {
-
-    si <- load_concepts("susp_inf", source, abx_min_count = 2L,
-      positive_cultures = TRUE, id_type = "icustay",
-      patient_ids = pids, si_mode = "or")
-
-  } else if (identical(source, "hirid")) {
-
-    si <- load_concepts("susp_inf", source, abx_min_count = 2L,
-      id_type = "icustay", patient_ids = pids,
-      si_mode = "or")
-
-  } else {
-
-    si <- load_concepts("susp_inf", source, id_type = "icustay",
-      patient_ids = pids)
-  }
-
-  sep3(load_concepts("sofa", source), si)
-}
