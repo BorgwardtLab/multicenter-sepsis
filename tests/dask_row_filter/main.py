7bc592b89aa6ecef1be20f00382eb83b68c3e4ed
mi92 <michael.d.moor@gmail.com>
2021-03-04 10:23:38 +0100

    added full comparison of row filtering with pandas, dask and pyarrow parquet ParquetDataset, only dask reads entire row group I believe

--- tests/dask_row_filter/main.py
+++ tests/dask_row_filter/main.py
@@ -1,5 +1,8 @@
 import dask.dataframe as dd
+import pandas as pd
+import pyarrow.parquet as pq
 
+from IPython import embed
 
 path = 'datasets/downloads/mimic_demo_0.3.0.parquet'
 
@@ -7,10 +10,17 @@ path = 'datasets/downloads/mimic_demo_0.3.0.parquet'
 df = dd.read_parquet(path, engine='pyarrow').compute()
 
 # generate subset of ids:
-ids = df['stay_id'].unique()[:5]
+ids = tuple(df['stay_id'].unique()[:5])
 
 # filter:
 filt = [('stay_id', 'in', ids)]
 
 # read subset of ids:
-df2 = dd.read_parquet(path, engine='pyarrow', filters=filt)
+df2 = dd.read_parquet(path, engine='pyarrow', filters=filt).compute()
+
+df3 = pd.read_parquet(path, engine='pyarrow', filters=filt)
+
+dataset = pq.ParquetDataset(path, use_legacy_dataset=False, filters=filt)
+df4 = dataset.read().to_pandas()
+
+embed()
