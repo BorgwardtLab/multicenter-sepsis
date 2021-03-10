fc8cffa978aba506bf897daa2c0581376c0fc22d
Max Horn <max.horn@bsse.ethz.ch>
2021-03-10 13:51:06 +0100

    Add normalization to torch datasets

--- tests/test_torch_datasets.py
+++ tests/test_torch_datasets.py
@@ -6,7 +6,7 @@ from src.torch.datasets import (
 @pytest.mark.parametrize(
     'dataset_cls', [MIMICDemo, Physionet2019, MIMIC, Hirid, EICU, AUMC])
 @pytest.mark.parametrize('split', ['train', 'test'])  # train and val use same code path
-@pytest.mark.parametrize('feature_set', ['small'])
+@pytest.mark.parametrize('feature_set', ['small', 'large'])
 @pytest.mark.parametrize('only_physionet_features', [False])
 @pytest.mark.parametrize('fold', [0])
 def test_read_first_instance(dataset_cls, split, feature_set, only_physionet_features, fold):
