4b8cb740a288089024391eebb99bf148c7de446c
Max Horn <max.horn@bsse.ethz.ch>
2021-03-11 13:00:02 +0100

    Extend tests to cover all scenarios

--- tests/test_torch_datasets.py
+++ tests/test_torch_datasets.py
@@ -7,7 +7,7 @@ from src.torch.datasets import (
     'dataset_cls', [MIMICDemo, Physionet2019, MIMIC, Hirid, EICU, AUMC])
 @pytest.mark.parametrize('split', ['train', 'test'])  # train and val use same code path
 @pytest.mark.parametrize('feature_set', ['small', 'large'])
-@pytest.mark.parametrize('only_physionet_features', [False])
+@pytest.mark.parametrize('only_physionet_features', [True, False])
 @pytest.mark.parametrize('fold', [0])
 def test_read_first_instance(dataset_cls, split, feature_set, only_physionet_features, fold):
     data = dataset_cls(split, feature_set, only_physionet_features, fold)
