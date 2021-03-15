625d39d9e26cda0aba5762a0d3ba97fc696f56ea
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-03-15 14:18:12 +0100

    overfitting test run

--- r/cube_setup.R
+++ r/cube_setup.R
@@ -3,7 +3,7 @@
 #BSUB -W 24:00
 #BSUB -n 16
 #BSUB -R rusage[mem=4000]
-#BSUB -J model[1-48]%24
+#BSUB -J model[1-2]
 #BSUB -o data-res/model_%J.out
 
 invisible(
@@ -12,14 +12,11 @@ invisible(
 
 args <- check_index(
   parse_args(job_index),
-  train_src = c("mimic", "aumc"),
-  feat_set = c("basic", "wav", "sig", "full"),
-  predictor = c("linear", "rf"),
-  target = c("class", "hybrid", "reg")
+  predictor = c("linear", "rf")
 )
 
 redir <- file.path(data_path("res"), paste0("model_", jobid()))
-extra <- list(test_src = c("mimic", "aumc"), res_dir = redir)
+extra <- list(train_src = "aumc", res_dir = redir)
 
 invisible(
   prof(do.call(fit_predict, c(args, extra)))
