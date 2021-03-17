3d95869e1bf8e4bf708b4003d0bcadaba40e791c
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-03-17 11:05:59 +0100

    update model fun

--- r/utils/model.R
+++ r/utils/model.R
@@ -1,7 +1,7 @@
 
 fit_predict <- function(train_src = "mimic_demo", test_src = train_src,
-                        feat_set = c("basic", "wav", "sig", "full"), feat_reg,
-                        predictor = c("linear", "rf"),
+                        feat_set = c("basic", "locf", "wav", "sig", "full"),
+                        feat_reg, predictor = c("linear", "rf"),
                         target = c("class", "hybrid", "reg"),
                         split = "split_0", data_dir = data_path("mm"),
                         res_dir = data_path("res"), ...) {
@@ -15,6 +15,7 @@ fit_predict <- function(train_src = "mimic_demo", test_src = train_src,
     feat_set <- match.arg(feat_set)
     feat_reg <- switch(feat_set,
       basic = "_(hours|locf|derived)$",
+      locf = "_(locf|derived)$",
       wav = "_(hours|locf|derived|wavelet_[0-9]+)$",
       sig = "_(hours|locf|derived|signature_[0-9]+)$",
       full = "_(hours|locf|derived|wavelet_[0-9]+|signature_[0-9]+)$"
@@ -31,23 +32,24 @@ fit_predict <- function(train_src = "mimic_demo", test_src = train_src,
   msg("training `", predictor, "` model on `", train_src, "` with `", target,
       "` response and using `", feat_set, "` feature set")
 
-  mat <- switch(predictor, rf = "mem", linear = "big")
   job <- file.path(res_dir, paste(predictor, target, feat_set, train_src,
                                   sep = "-"))
 
   y <- switch(target,
-    class = y_class(train_src, hours(6L), hours(Inf), path = data_dir,
+    class = y_class(train_src, 6L, Inf, path = data_dir,
                     split = split),
-    hybrid = y_class(train_src, hours(6L), hours(6L), path = data_dir,
+    hybrid = y_class(train_src, 6L, 6L, path = data_dir,
                      split = split),
     reg = y_reg(train_src, path = data_dir, split = split)
   )
 
   msg("reading train data")
 
+  read_x_fun <- switch(predictor, rf = read_to_mat, linear = read_to_bm)
+
   x <- prof(
-    read_to_mat(train_src, cols = feature_sel(feat_reg, predictor),
-      path = data_dir, split = split, mat_type = mat
+    read_x_fun(train_src, cols = feature_sel(feat_reg, predictor),
+      path = data_dir, split = split
     )
   )
 
@@ -67,8 +69,8 @@ fit_predict <- function(train_src = "mimic_demo", test_src = train_src,
     msg("reading `", src, "` validation data")
 
     x <- prof(
-      read_to_mat(src, cols = feature_sel(feat_reg, predictor),
-        path = data_dir, split = split, pids = pids, mat_type = mat
+      read_x_fun(src, cols = feature_sel(feat_reg, predictor),
+        path = data_dir, split = split, pids = pids
       )
     )
 
@@ -78,9 +80,9 @@ fit_predict <- function(train_src = "mimic_demo", test_src = train_src,
     res <- prof(fun(mod, x))
 
     y <- switch(target,
-      class = y_class(src, hours(6L), hours(Inf), path = data_dir,
+      class = y_class(src, 6L, Inf, path = data_dir,
                       split = split, pids = pids),
-      hybrid = y_class(src, hours(6L), hours(6L), path = data_dir,
+      hybrid = y_class(src, 6L, 6L, path = data_dir,
                        split = split, pids = pids),
       reg = y_reg(src, path = data_dir, split = split, pids = pids)
     )
