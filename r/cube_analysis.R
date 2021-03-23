26bc366aeab9523d6075e031aafd89492ad713c0
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-03-23 09:37:58 +0100

    cube plot to pdf

--- r/cube_analysis.R
+++ r/cube_analysis.R
@@ -4,10 +4,10 @@ invisible(
 )
 
 dims <- list(
-  train_src = "aumc",
+  train_src = c("aumc", "mimic"),
   feat_set = c("locf", "basic", "wav", "sig", "full"),
   predictor = c("rf", "linear"),
-  target = c("class", "hybrid")
+  target = c("class", "hybrid", "reg")
 )
 
 res <- array(vector("list", prod(lengths(dims))), lengths(dims), dims)
@@ -21,8 +21,12 @@ for (train in dims$train_src) {
         tmp <- read_res(train, feat_set = feat, predictor = pred,
                         target = targ, jobid = 165674415)
         if (is.null(tmp)) next
+        pat_eval <- patient_eval(tmp)
         res[train, feat, pred, targ] <- list(tmp)
-        evl[train, feat, pred, targ] <- list(patient_eval(tmp))
+        evl[train, feat, pred, targ] <- list(pat_eval)
+        plt <- patient_plot(pat_eval, pred)
+        ggsave(paste0(
+          paste(pred, targ, feat, train, train, sep = "-"), ".pdf"), plt)
       }
     }
   }
