433d4ea405df911a0372727aec3ca54ceb3e85a3
Max Horn <max.horn@bsse.ethz.ch>
2021-03-23 21:23:43 +0100

    Add tests for sorting of datasets

--- tests/test_dataset_sorting.py
+++ tests/test_dataset_sorting.py
@@ -2,17 +2,62 @@ import pytest
 import pyarrow.parquet as pq
 import numpy as np
 
+
 @pytest.mark.parametrize(
     'dataset_path',
     [
-        'datasets/physionet2019/data/parquet/features',
+        'datasets/downloads/aumc_0.3.0.parquet',
+        'datasets/downloads/eicu_0.3.0.parquet',
+        'datasets/downloads/eicu_demo_0.3.0.parquet',
+        'datasets/downloads/hirid_0.3.0.parquet',
+        'datasets/downloads/mimic_0.3.0.parquet',
+        'datasets/downloads/mimic_demo_0.3.0.parquet',
+        'datasets/downloads/physionet2019_0.3.0.parquet'
+    ]
+)
+def test_sorted_ids_and_times_raw(dataset_path):
+    table = pq.read_table(dataset_path, columns=['stay_id', 'stay_time'])
+    stay_ids = table['stay_id'].to_numpy().astype(int)
+    # times = table['stay_time'].to_numpy().astype(float)
+    diffs = np.diff(stay_ids)
+    positions = np.where(diffs < 0)[0]
+    # print(positions)
+    if len(positions) > 0:
+        print(stay_ids[positions[0] - 2:positions[0]+3])
+        print(diffs[positions[0] - 2:positions[0]+3])
+        unique, indices = np.unique(stay_ids, return_index=True)
+        print(unique[np.argsort(indices)])
+    assert not np.any(diffs < 0)
+
+
+@pytest.mark.parametrize(
+    'dataset_path',
+    [
         'datasets/mimic_demo/data/parquet/features',
+        'datasets/physionet2019/data/parquet/features',
         'datasets/mimic/data/parquet/features',
         'datasets/hirid/data/parquet/features',
         'datasets/eicu/data/parquet/features',
         'datasets/aumc/data/parquet/features'
     ]
 )
-def test_consecutive_sorted_ids(dataset_path):
+def test_sorted_ids_and_times_features(dataset_path):
     table = pq.read_table(dataset_path, columns=['stay_id', 'stay_time'])
-    assert np.all(np.diff(table['stay_id'].to_numpy()) > 0)
+    stay_ids = table['stay_id'].to_numpy().astype(int)
+    diffs = np.diff(stay_ids)
+    positions = np.where(diffs < 0)[0]
+    # print(positions)
+    if len(positions) > 0:
+        print(stay_ids[positions[0] - 2:positions[0]+3])
+        print(diffs[positions[0] - 2:positions[0]+3])
+        unique, indices = np.unique(stay_ids, return_index=True)
+        print(unique[np.argsort(indices)])
+    assert not np.any(diffs < 0)
+    # times = table['stay_time'].to_numpy().astype(float)
+    # unique_stays, inverse, counts = np.unique(stay_ids, return_inverse=True, return_counts=True)
+    # time_offsets = np.roll(np.cumsum(counts), 1)
+    # time_offsets[0] = 0
+    # time_diffs = np.diff(times + time_offsets[inverse])
+    # assert not np.any(time_diffs < 0)
+
+
--- tests/test_torch_datasets.py
+++ tests/test_torch_datasets.py
@@ -8,8 +8,10 @@ from src.torch.datasets import (
 @pytest.mark.parametrize(
     'dataset_cls', [MIMICDemo, Physionet2019, MIMIC, Hirid, EICU, AUMC])
 @pytest.mark.parametrize('split', ['train', 'test'])  # train and val use same code path
-@pytest.mark.parametrize('feature_set', ['small', 'large'])
-@pytest.mark.parametrize('only_physionet_features', [True, False])
+# @pytest.mark.parametrize('feature_set', ['small', 'large'])
+# @pytest.mark.parametrize('only_physionet_features', [true, false])
+@pytest.mark.parametrize('feature_set', ['small'])
+@pytest.mark.parametrize('only_physionet_features', [False])
 @pytest.mark.parametrize('fold', [0])
 def test_read_first_instance(dataset_cls, split, feature_set, only_physionet_features, fold):
     data = dataset_cls(split, feature_set, only_physionet_features, fold)
@@ -18,9 +20,9 @@ def test_read_first_instance(dataset_cls, split, feature_set, only_physionet_fea
 
 @pytest.mark.parametrize(
     'dataset_cls', [MIMICDemo, Physionet2019, MIMIC, Hirid, EICU, AUMC])
-@pytest.mark.parametrize('split', ['train', 'test'])  # train and val use same code path
-@pytest.mark.parametrize('feature_set', ['small', 'large'])
-@pytest.mark.parametrize('only_physionet_features', [True, False])
+@pytest.mark.parametrize('split', ['train'])  # train and val use same code path
+@pytest.mark.parametrize('feature_set', ['small'])
+@pytest.mark.parametrize('only_physionet_features', [False])
 @pytest.mark.parametrize('fold', [0])
 def test_random_access_speed(dataset_cls, split, feature_set, only_physionet_features, fold):
     data = dataset_cls(split, feature_set, only_physionet_features, fold)
@@ -29,6 +31,6 @@ def test_random_access_speed(dataset_cls, split, feature_set, only_physionet_fea
     time_per_instance = timeit.timeit(
         'data[rand.randint(0, max_index)]',
         globals={'data': data, 'max_index': max_index, 'rand': rand},
-        number=1000
-    ) / 1000
+        number=10
+    ) / 10
     assert time_per_instance < 0.1
