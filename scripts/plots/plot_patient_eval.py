017bbc555a2d3746c9c9cb2fbfb8cc294410df43
mi92 <michael.d.moor@gmail.com>
2021-04-20 13:57:07 +0200

    checking in update to patient eval plotting (inclduing for now hardcoded prevalences)

--- scripts/plots/plot_patient_eval.py
+++ scripts/plots/plot_patient_eval.py
@@ -23,10 +23,33 @@ import seaborn as sns
 
 import pandas as pd
 import numpy as np
+from src.evaluation.patient_evaluation import format_dataset
 
+# hard-coded prevalences (for now):
+prev_dict = {'physionet2019': {'validation': {'tp_prevalence': 0.005078736911534339,
+   'case_prevalence': 0.052750992626205334,
+   'total_stays': 1763,
+   'total_cases': 93}},
+ 'mimic': {'validation': {'tp_prevalence': 0.12132375975840508,
+   'case_prevalence': 0.2606824469720258,
+   'total_stays': 3253,
+   'total_cases': 848}},
+ 'eicu': {'validation': {'tp_prevalence': 0.03642906177626566,
+   'case_prevalence': 0.08283789139912802,
+   'total_stays': 5046,
+   'total_cases': 418}},
+ 'hirid': {'validation': {'tp_prevalence': 0.19987524505435753,
+   'case_prevalence': 0.37278350515463915,
+   'total_stays': 2425,
+   'total_cases': 904}},
+ 'aumc': {'validation': {'tp_prevalence': 0.055438335195068474,
+   'case_prevalence': 0.0801987224982257,
+   'total_stays': 1409,
+   'total_cases': 113}}}
 
-def plot_curves(df, ax, names={}):
+def plot_curves(df, ax, names={}, level='pat'):
     """Plot curve-based evaluation."""
+    assert level in ['pat', 'tp'] 
     # We will need to mirror this axis later on.
     ax1 = ax
     model = names['model']
@@ -36,10 +59,14 @@ def plot_curves(df, ax, names={}):
     ax1.set_title(f'{model}, trained for {task} on {train_dataset}, applied to {eval_dataset}', fontsize=17)
     ax1.set_xlabel('Decision threshold', fontsize=14)
     ax1.set_ylabel('Score', fontsize=14, color='green')
-    ax1 = sns.lineplot(x='thres', y='pat_precision', data = df,
+    ax1 = sns.lineplot(x='thres', y=level+'_precision', data = df,
             label='precision', color='darkgreen', ax=ax1)
-    ax1 = sns.lineplot(x='thres', y='pat_recall', data = df,
+    ax1 = sns.lineplot(x='thres', y=level+'_recall', data = df,
             label='recall', color='lightgreen', ax=ax1) #tab:green
+    if level == 'pat':
+        ax1 = sns.lineplot(x='thres', y='pat_specificity', data = df,
+            label='specificity', color='lightblue', ax=ax1)
+ 
     #ax1 = sns.lineplot(x='thres', y='physionet2019_utility', data = df,
     #        label='utility', color='black', ax=ax1)
 
@@ -72,22 +99,35 @@ def plot_scores(df, scores, ax, **kwargs):
     ax.set_xlim((xmin, xmax))
 
 
-def plot_info(df, ax, recall_threshold=0.90):
+def plot_info(df, ax, recall_threshold=0.90, level='pat'):
     """Plot information textbox."""
     # Get precision at the pre-defined recall threshold (typically 90%,
     # but other values might be interesting).
-    greater = df['pat_recall'] > recall_threshold
-    index = df['pat_recall'][greater].argmin()
+    assert level in ['pat', 'tp']
+    level_name = 'Patient-based' if level == 'pat' else 'Timepoint-based'
+    precision = level+'_precision'
+    recall = level+'_recall'
+    greater = df[recall] > recall_threshold
+    index = df[recall][greater].argmin()
     info = df.loc[index]
 
     earliness = f'earliness_{args.earliness_stat}'
-
-    textbox = '\n'.join((
-        f'Patient-based Recall:    {info["pat_recall"]:5.2f}',
-        f'Patient-based Precision: {info["pat_precision"]:5.2f}',
+    if level == 'pat':
+        textbox = '\n'.join((
+            f'{level_name} Recall:    {info[recall]:5.2f}',
+            f'{level_name} Precision: {info[precision]:5.2f}',
+            f'{level_name} Specificity: {info["pat_specificity"]:5.2f}',
+            f'Threshold:               {info["thres"]:5.4f}',
+            f'Earliness {args.earliness_stat}: {info[earliness]:5.2f}',
+        ))
+    else:
+        textbox = '\n'.join((
+        f'{level_name} Recall:    {info[recall]:5.2f}',
+        f'{level_name} Precision: {info[precision]:5.2f}',
         f'Threshold:               {info["thres"]:5.4f}',
-        f'Earliness {args.earliness_stat}:           {info[earliness]:5.2f}',
+        f'Earliness {args.earliness_stat}: {info[earliness]:5.2f}',
     ))
+
 
     ax.set_xticks([])
     ax.set_yticks([])
@@ -151,6 +191,9 @@ if __name__ == '__main__':
     parser.add_argument('--predictions_path', required=True)
     parser.add_argument('--output_path', default='results/evaluation/plots')
     parser.add_argument('--earliness-stat', default='mean')
+    parser.add_argument('--level', default='pat', help='pat or tp level')
+    parser.add_argument('--recall_thres', default=0.9, type=float, help='recall threshold [0-1]')
+
     parser.add_argument('-s', '--show', action='store_true')
 
     args = parser.parse_args()
@@ -158,6 +201,8 @@ if __name__ == '__main__':
     with open(input_path, 'r') as f:
         d = json.load(f)
     df = pd.DataFrame(d)
+    level = args.level
+    recall_thres = args.recall_thres
 
     with open(args.predictions_path, 'r') as f:
         d = json.load(f)
@@ -174,14 +219,24 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(nrows=4, figsize=(9, 11), squeeze=True) #6,7
 
     # for setting title:
-    xmin, xmax = plot_curves(df, axes[0], names)
+    xmin, xmax = plot_curves(df, axes[0], names,level=level)
     plot_scores(df, scores, axes[1])
     plot_proportion_curves(df, axes[2])
-    info = plot_info(df, axes[3])
+    info = plot_info(df, axes[3], level=level, recall_threshold=recall_thres)
+    # vertical cut-off:
     for ax in axes[:3]:
         ax.axvline(info['thres'], color='black')
-
-    out_file = os.path.split(input_path)[-1].split('.')[0] + '_' + earliness + '.png' 
+    # prevalence hline:
+    if level == 'pat':
+        prev = 'case_prevalence'
+    elif level == 'tp':
+        prev = 'tp_prevalence'
+    axes[0].axhline(
+        prev_dict[format_dataset(names['eval_dataset'])]['validation'][prev], 
+        linestyle='--', color='black'
+    )
+ 
+    out_file = os.path.split(input_path)[-1].split('.')[0] + f'_{earliness}_{level}_thres_{100*recall_thres}.png' 
     plt.tight_layout()
     plt.savefig( os.path.join(args.output_path, out_file))
 
