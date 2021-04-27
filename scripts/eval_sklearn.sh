4d6b538e621be017f8d9cb407fcfb21b33cbf80e
mi92 <michael.d.moor@gmail.com>
2021-04-27 12:19:23 +0200

    updating sklearn eval script to loop over method and baselines

--- scripts/eval_sklearn.sh
+++ scripts/eval_sklearn.sh
@@ -5,49 +5,59 @@ input_dir=$1 # path to hyperparameter search results e.g. hypersearch10_regressi
 base_dir=results
 eval_dir=${base_dir}/evaluation
 
-
-train_datasets=(hirid aumc mimic eicu) #aumc #mimic
-eval_datasets=(hirid aumc mimic eicu) #aumc #mimic
-method=lgbm #lgbm
-task=regression #classification 
+train_datasets=(aumc hirid mimic eicu) #aumc eicu) #aumc #mimic
+eval_datasets=(aumc hirid mimic eicu) #aumc #mimic
+methods=(lgbm sofa qsofa sirs mews news) #sofa
+task=classification 
 feature_set=middle #large
 cost=5 #lambda cost
 n_iter=50 #50 iterations of hypersearch used
 earliness=median
+thres=0.8
 
-for train_dataset in ${train_datasets[@]}; do
-    for eval_dataset in ${eval_datasets[@]}; do
-        output_dir=${method}_${train_dataset}_${eval_dataset}_${task}_${feature_set}_cost_${cost}_${n_iter}_iter 
-        # here we write out the predictions as json
-        pred_file=${eval_dir}/prediction_output/${output_dir}.json
-        #pred_file=$pred_dir/${method}_${train_dataset}_${eval_dataset}.json
-        # here we write out the evaluation metrics as json
-        eval_file=${eval_dir}/evaluation_output/${output_dir}.json
+input_features=datasets/{}/data/parquet/features_${feature_set}
 
-        # Apply pretrained model to validation (or test) split:
-        python -m src.sklearn.eval_model \
-            --model_path $input_dir \
-            --output_path $pred_file \
-            --method $method \
-            --task $task \
-            --train_dataset $train_dataset \
-            --eval_dataset $eval_dataset \
-            --feature_set $feature_set \
-            --cost $cost
+for method in ${methods[@]}; do
+    for rep in {0..4}; do
+        for train_dataset in ${train_datasets[@]}; do
+            for eval_dataset in ${eval_datasets[@]}; do
+                output_dir=${method}_${train_dataset}_${eval_dataset}_${task}_${feature_set}_cost_${cost}_${n_iter}_iter_rep_${rep} 
+                # here we write out the predictions as json
+                pred_file=${eval_dir}/prediction_output/${output_dir}.json
+                #pred_file=$pred_dir/${method}_${train_dataset}_${eval_dataset}.json
+                # here we write out the evaluation metrics as json
+                eval_file=${eval_dir}/evaluation_output/${output_dir}.json
 
-        # Patient-based Evaluation:
-        python -m src.evaluation.patient_evaluation \
-            --input-file $pred_file \
-            --output-file $eval_file \
-            --n_jobs=50 \
-            --force \
-            --cost $cost
+                # Apply pretrained model to validation (or test) split:
+                python -m src.sklearn.eval_model \
+                    --input_path $input_features \
+                    --model_path $input_dir \
+                    --output_path $pred_file \
+                    --method $method \
+                    --task $task \
+                    --train_dataset $train_dataset \
+                    --eval_dataset $eval_dataset \
+                    --feature_set $feature_set \
+                    --cost $cost \
+                    --repetition_model \
+                    --rep $rep
 
-        # Plot patient-based eval metrics:
-        python scripts/plots/plot_patient_eval.py \
-            --input_path $eval_file  \
-            --output_path results/evaluation/plots \
-            --earliness-stat $earliness \
-            --predictions_path $pred_file
+                # Patient-based Evaluation:
+                python -m src.evaluation.patient_evaluation \
+                    --input-file $pred_file \
+                    --output-file $eval_file \
+                    --n_jobs=50 \
+                    --force \
+                    --cost $cost
+
+                # Plot patient-based eval metrics:
+                python -m scripts.plots.plot_patient_eval \
+                    --input_path $eval_file  \
+                    --output_path results/evaluation/plots \
+                    --earliness-stat $earliness \
+                    --predictions_path $pred_file \
+                    --recall_thres $thres
+            done
+        done
     done
 done
