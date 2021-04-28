912d80d8f9448919117388f8780e0048cbc7fbd9
mi92 <michael.d.moor@gmail.com>
2021-04-28 15:37:00 +0200

    updated gather_data script to scratch together raw result data

--- scripts/plots/gather_data.py
+++ scripts/plots/gather_data.py
@@ -20,7 +20,10 @@ def make_scatter(df, fname, signature, title=None):
  
 def process_run(eval_file, pred_file, recall_threshold=0.90):
     with open(eval_file, 'r') as f:
+        try:
             d_ev = json.load(f)
+        except: 
+            print(eval_file)
     df = pd.DataFrame(d_ev)
     with open(pred_file, 'r') as f:
         d_p = json.load(f)
@@ -38,24 +41,24 @@ def process_run(eval_file, pred_file, recall_threshold=0.90):
         df[key] = pred_dict[key]
     if 'task' not in d_p.keys():
         raise ValueError('Task information is not available!')
-        ##TODO: ADD TASK IN DEEP JSONS!!
-        #if any([id in d_p['run_id'] for id in ['r68b6gm4', 's453x0n9' ]]):
-        #    task  = 'regression'
-        #else: 
-        #    task = 'classification'
-        #pred_dict['task'] = task 
     try: result.update(pred_dict)
     except: #task not available in deep jsons
-        print(pred_file) 
+        print(pred_file)
+    #TODO: currently result dict not used 
     return df
 
 if __name__ == "__main__":
+    methods = ['lgbm', 'sofa', 'qsofa', 'sirs'] #attn model not in file name
+    attn_datasets = ['EICU', 'AUMC', 'Hirid', 'MIMIC'] 
+    #attn file names have this dataset namings (from datset cls)
+    keys = methods + attn_datasets
+ 
     path = 'results/evaluation/evaluation_output'
     eval_files = [os.path.join(p, f) for (p, _, files) in os.walk(path) for f in files]
     pred_files = [f.replace('evaluation_output', 'prediction_output') for f in eval_files] 
     df_list = [] 
     for i, (eval_f, pred_f) in enumerate(zip(eval_files, pred_files)):
-        if 'classification' in eval_f:
+        if any([x in eval_f for x in keys]):
             curr_df = process_run(eval_f, pred_f)
             curr_df['fname'] = eval_f
             curr_df['job_id'] = i
@@ -67,4 +70,4 @@ if __name__ == "__main__":
     outpath ='results/evaluation/plots'
     df.to_csv(os.path.join(outpath, 'result_data.csv'), index=False)
   
- 
+    embed() 
