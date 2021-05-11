af2791b78736b5a13c077d4e5096ba8ce14047c7
mi92 <michael.d.moor@gmail.com>
2021-05-11 18:27:18 +0200

    checking in submit eval bash script

--- scripts/wandb/submit_evals.sh
+++ scripts/wandb/submit_evals.sh
@@ -7,7 +7,7 @@ split=validation
 datasets=(AUMC MIMIC Hirid EICU)
 for dataset in ${datasets[@]}; do
     pred_file=results/evaluation/prediction_output/${run_id}_${dataset}.json
-    sbatch -p gpu --gres=gpu:1 --cpus-per-task 4 --mem-per-cpu 6G --exclude=bs-gpu07 -J 'mcsepsis' --wrap "pipenv run python -m src.torch.eval_model_wandb ${wandb_run} --dataset ${dataset} --split ${split} --output ${pred_file}"
+    sbatch -p gpu --gres=gpu:1 --cpus-per-task 4 --mem-per-cpu 6G  -J 'mcsepsis' --wrap "pipenv run python -m src.torch.eval_model_wandb ${wandb_run} --dataset ${dataset} --split ${split} --output ${pred_file}"
 done
 # MEMORY:
 # non-EICU: 10 parallel, 4cpu 4G
