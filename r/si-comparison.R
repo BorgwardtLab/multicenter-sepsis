5ef3960139466f9d7ad63d1239e637b34eb931c9
Drago Plecko <drago.plecko@stat.math.ethz.ch>
2021-05-19 14:35:32 +0200

    Update on patient table and venn diagram

--- r/si-comparison.R
+++ r/si-comparison.R
@@ -18,62 +18,46 @@ diff_summary <- function(tbl1, tbl2, plt.name, plot.main) {
   myCol <- c("#B3E2CD", "#FDCDAC") # brewer.pal(2, "Pastel2")
   
   # Chart
-  venn.diagram(
-    x = list(A, B),
-    category.names = c("SI + ABX" , "ABX"),
-    filename = plt.name,
-    output=TRUE,
-    main = plot.main,
-    
-    # Output features
-    imagetype="png" ,
-    height = 600 , 
-    width = 800 , 
-    resolution = 300,
-    compression = "lzw",
-    
-    # Circles
-    lwd = 2,
-    lty = 'blank',
-    fill = myCol,
-    
-    # Numbers
-    cex = .6,
-    fontface = "bold",
-    fontfamily = "sans",
-    
-    # Set names
-    cat.cex = 0.6,
-    cat.fontface = "bold",
-    cat.default.pos = "outer",
-    cat.pos = c(-27, 27),
-    cat.dist = c(0.055, 0.055),
-    cat.fontfamily = "sans"
-  )
+  venn.diagram(x = list(A, B), fill = c("lightblue", "green"), 
+               category.names = c("SI + ABX", "multi-ABX"),
+               alpha = c(0.5, 0.5), lwd=0.1, 
+               height = 1200, 
+               width = 1200,
+               main.cex = 2,
+               cat.pos = c(-30, 40),
+               resolution = 300,
+               main = plot.main,
+               compression = "lzw",
+               filename = plt.name)
   
   return(list(A, B, C))
   
 }
 
-si_cmp <- function(src, min.age = 14L) {
+si_cmp <- function(src) {
   
-  beau <- function(src) ifelse(src == "mimic", "MIMIC-III", ifelse(src == "aumc", "AUMC", "wrong dataset"))
+  beau <- function(src) ifelse(src == "mimic", "MIMIC-III", 
+                               ifelse(src == "aumc", "AUMC", "wrong dataset"))
   
-  orig <- load_concepts("susp_inf", src)
+  patient_ids <- read_json(file.path(root, "config", "splits",
+                                     paste0("splits_", src, ".json")), 
+                           simplifyVector = T)[["total"]][["ids"]]
+  
+  orig <- load_concepts("susp_inf", src, patient_ids = patient_ids)
   orig <- orig[, meta_vars(orig), with = FALSE]
   
-  abx <- ricu:::si_abx(load_concepts("abx", src), hours(24L), 2L) 
+  abx <- ricu:::si_abx(load_concepts("abx", src, patient_ids = patient_ids), 
+                       hours(24L), 2L) 
   # load_concepts("susp_inf", src, abx_min_count = 2L, si_mode = "or")
   
   abx <- abx[is_true(abx)]
   abx[, abx := NULL]
   abx <- abx[, meta_vars(abx), with = FALSE]
   
-  adults <- id_col(load_concepts("age", src)[age > min.age])
-  orig <- orig[id_col(orig) %in% adults]
-  abx <- abx[id_col(abx) %in% adults]
+  orig <- orig[id_col(orig) %in% patient_ids]
+  abx <- abx[id_col(abx) %in% patient_ids]
   
-  diff <- diff_summary(orig, abx, paste0("si_comparison_", src, ".png"), beau(src))
+  diff <- diff_summary(orig, abx, paste0("si_comparison_", src, ".tiff"), beau(src))
   
 }
 
