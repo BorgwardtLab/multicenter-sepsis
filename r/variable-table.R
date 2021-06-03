f9767ddb2296535b7368b48ab806fbb02a35e838
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-06-03 10:55:08 +0200

    improve vars tbl

--- r/variable-table.R
+++ r/variable-table.R
@@ -3,6 +3,13 @@ invisible(
   lapply(list.files(here::here("r", "utils"), full.names = TRUE), source)
 )
 
+# needs:
+# \usepackage{booktabs}
+# \usepackage{longtable}
+# \usepackage{pifont}
+# \usepackage{makecell}
+
+
 ynm <- function(x) {
   ifelse(is_true(x), "\\ding{51}",ifelse(is_false(x), "\\ding{55}", "-"))
 }
@@ -23,6 +30,11 @@ res <- merge(explain_dictionary(dict, cols = c("name", "description")), res,
 res[, srcs] <- lapply(res[, srcs], ynm)
 res <- res[, c("name", "description", srcs)]
 res$name <- sub("_", "\\_", res$name, fixed = TRUE)
+res$description <- kableExtra::linebreak(
+  vapply(strwrap(res$description, 35, simplify = FALSE), paste, character(1L),
+         collapse = "\n"),
+  align = "l"
+)
 colnames(res) <- c("Name", "Description", "MIMIC-III", "eICU", "HiRID", "AUMC")
 
 kableExtra::kbl(res, "latex", booktabs = TRUE, longtable = TRUE, escape = FALSE,
