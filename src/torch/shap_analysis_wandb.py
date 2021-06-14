74d787f91e35986cdad17ba07d79f0afb3817c04
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-06-14 16:19:04 +0200

    Created separate utilities file for Shapley values
    We will need this in order to simplify the visualisation and analysis of
    these values later on.

--- src/torch/shap_analysis_wandb.py
+++ src/torch/shap_analysis_wandb.py
@@ -9,13 +9,12 @@ import warnings
 
 import numpy as np
 
+from src.torch.eval_model import device
+
 from torch.utils.data import Subset
 from torch.utils.data import SubsetRandomSampler
 
-from src.torch.eval_model import compute_md5hash, device
-from src.torch.torch_utils import ComposeTransformations
-import src.torch.datasets
-import src.torch.models
+from src.torch.shap_utils import get_model_and_dataset
 
 import shap
 
@@ -126,61 +125,6 @@ def variable_length_collate_nan_padding(batch, lengths=None):
         key: torch.from_numpy(data) for key, data in
         transposed_items.items()
     }
-
-
-def extract_model_information(run_path, tmp):
-    """Get model information from wandb run."""
-    run = wandb_api.run(run_path)
-    run_info = run.config
-    checkpoint_path = None
-    for f in run.files():
-        if f.name.endswith('.ckpt'):
-            file_desc = f.download(tmp)
-            checkpoint_path = file_desc.name
-            file_desc.close()
-    if checkpoint_path is None:
-        raise RuntimeError(
-            f'Run "{run_path}" does not have a stored checkpoint file.')
-
-    model_checksum = compute_md5hash(checkpoint_path)
-    dataset_kwargs = {}
-    for key in run_info.keys():
-        if 'dataset_kwargs' in key:
-            new_key = key.split('/')[-1]
-            dataset_kwargs[new_key] = run_info[key]
-    return run, {
-        "model": run_info['model'],
-        "run_id": run_path,
-        "model_path": checkpoint_path,
-        "model_checksum": model_checksum,
-        "model_params": run_info,
-        "dataset_train": run_info['dataset'],
-        "task": run_info['task'],
-        "label_propagation": run_info['label_propagation'],
-        "rep": run_info['rep'],
-        "dataset_kwargs": dataset_kwargs
-    }
-
-
-def get_model_and_dataset(run_id):
-    """Get model and dataset from finished wandb run."""
-    with tempfile.TemporaryDirectory() as tmp:
-        # Download checkpoint to temporary directory
-        run, out = extract_model_information(run_id, tmp)
-
-        model_cls = getattr(src.torch.models, out['model'])
-        model = model_cls.load_from_checkpoint(
-            out['model_path'],
-            dataset=out['dataset_train']
-        )
-    model.to(device)
-    dataset = model.dataset_cls(
-        split='test',
-        transform=ComposeTransformations(model.transforms),
-        **out['dataset_kwargs']
-    )
-
-    return model, dataset
 
 
 def get_feature_names(dataset):
