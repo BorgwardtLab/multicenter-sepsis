dd6abf5dad529a331d0b3c9a611d02fa60dea03a
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-06-28 15:06:59 +0200

    Removed obsolete ranking script

--- scripts/rank_shapley.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Calculate rankings of the Shapley features. Unlike the visualisation
-# scenario, this one runs faster.
-
-HOURS=16
-
-python -m src.torch.rank_shapley_values results/shapley/*.pkl -H ${HOURS}
