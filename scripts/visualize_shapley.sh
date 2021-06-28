82d6fcc300cf5ad0db35950a6dd9d2159bd0c112
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-06-28 15:43:36 +0200

    Updated Shapley calculation script

--- scripts/visualize_shapley.sh
+++ scripts/visualize_shapley.sh
@@ -6,16 +6,16 @@
 
 HOURS=16
 
+echo 'Calculating bar plot and rankings...'
+
+python -m scripts.visualize_shapley_bar results/shapley/0zcdmr2b_*.pkl \
+                                        results/shapley/4ky293gb_*.pkl \
+                                        results/shapley/9rqxww43_*.pkl \
+                                        results/shapley/hacl0kfp_*.pkl \
+                                        -H ${HOURS}
+
 echo 'Calculating swarm plots...'
 
 for RUN in '0zcdmr2b' '4ky293gb' '9rqxww43' 'hacl0kfp'; do
   python -m scripts.visualize_shapley_swarm results/shapley/${RUN}_*.pkl -H ${HOURS}
 done
-
-echo 'Calculating bar plot...'
-
-python -m scripts.visualize_shapley_swarm results/shapley/0zcdmr2b_*.pkl \
-                                          results/shapley/4ky293gb_*.pkl \
-                                          results/shapley/9rqxww43_*.pkl \
-                                          results/shapley/hacl0kfp_*.pkl \
-                                          -H ${HOURS}
