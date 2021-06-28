8fc496d3d0f8770d9de1d97d0bff394b32bae659
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-06-28 10:50:57 +0200

    Simplified Shapley ranking script

--- scripts/rank_shapley.sh
+++ scripts/rank_shapley.sh
@@ -3,7 +3,6 @@
 # Calculate rankings of the Shapley features. Unlike the visualisation
 # scenario, this one runs faster.
 
-python -m src.torch.rank_shapley_values results/shapley/*.pkl -c
-for HOURS in 1 2 4 8 16; do
-  python -m src.torch.rank_shapley_values results/shapley/*.pkl -H ${HOURS} -c
-done
+HOURS=16
+
+python -m src.torch.rank_shapley_values results/shapley/*.pkl -H ${HOURS}
