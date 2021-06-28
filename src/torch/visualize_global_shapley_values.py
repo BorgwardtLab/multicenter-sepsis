df34fa6a508ab0ddb4b8cd80d49a221d996b6e12
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-06-28 11:35:00 +0200

    Refactored global plotting script for Shapley values
    This script should only provide a 'bee swarm' plot, calculated for
    a *single* data set. Will update the script's name afterwards.

--- src/torch/visualize_global_shapley_values.py
+++ src/torch/visualize_global_shapley_values.py
@@ -1,216 +1,14 @@
 """Visualisation of global Shapley values."""
 
 import argparse
-import io
 import os
-import pickle
 import shap
-import torch
 
 import numpy as np
-import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from src.torch.shap_utils import get_model_and_dataset
-
-
-class PickledTorch(pickle.Unpickler):
-    """Unpickler for `torch` objects.
-
-    Simple wrapper for `torch` objects that need to be serialised to
-    a CPU instead of a GPU. Permits the analysis of such files, even
-    if CUDA is not present.
-    """
-
-    def find_class(self, module, name):
-        """Return class for handling functions of a certain type."""
-        if module == 'torch.storage' and name == '_load_from_bytes':
-            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
-        else:
-            return super().find_class(module, name)
-
-
-def get_run_id(filename):
-    """Extract run ID from filename."""
-    run_id = os.path.basename(filename)
-    run_id = os.path.splitext(run_id)[0]
-    run_id = run_id[:8]
-    run_id = f'sepsis/mc-sepsis/runs/{run_id}'
-    return run_id
-
-
-def pool(lengths, shapley_values, feature_values, hours_before=None):
-    """Pool Shapley values and feature values.
-
-    This function performs the pooling step required for Shapley values
-    and feature values. Each time step of a time series will be treated
-    as its own sample instance.
-
-    Parameters
-    ----------
-    lengths:
-        Vector of lengths.
-
-    shapley_values:
-        Shapley values.
-
-    feature_values:
-        Feature values.
-
-    hours_before : int or `None`, optional
-        If not `None`, only uses (at most) the last `hours_before`
-        observations when reporting the Shapley values.
-
-    Returns
-    -------
-    Tuple of pooled Shapely values and feature values.
-    """
-    # This is the straightforward way of doing it. Please don't judge,
-    # or, if you do, don't judge too much :)
-    shapley_values_pooled = []
-    feature_values_pooled = []
-
-    for index, (s_values, f_values) in enumerate(
-        zip(shapley_values, feature_values)
-    ):
-        length = lengths[index]
-
-        # Check whether we can remove everything but the last hours
-        # before the maximum prediction score.
-        if hours_before is not None:
-            if length >= hours_before:
-                s_values = s_values[length - hours_before:length, :]
-                f_values = f_values[length - hours_before:length, :]
-
-        # Just take *everything*.
-        else:
-            s_values = s_values[:length, :]
-            f_values = f_values[:length, :]
-
-        # Need to store mask for subsequent calculations. This is
-        # required because we must only select features for which
-        # the Shapley value is defined!
-        mask = ~np.isnan(s_values).all(axis=1)
-
-        s_values = s_values[mask, :]
-        f_values = f_values[mask, :]
-
-        shapley_values_pooled.append(s_values)
-        feature_values_pooled.append(f_values)
-
-    shapley_values_pooled = np.vstack(shapley_values_pooled)
-    feature_values_pooled = np.vstack(feature_values_pooled)
-
-    return shapley_values_pooled, feature_values_pooled
-
-
-def get_aggregation_function(name):
-    """Return aggregation function."""
-    if name == 'absmax':
-        def absmax(x):
-            return max(x.min(), x.max(), key=abs)
-        return absmax
-    elif name == 'max':
-        return np.max
-    elif name == 'min':
-        return np.min
-    elif name == 'mean':
-        return np.mean
-    elif name == 'median':
-        return np.median
-
-
-def make_explanation(shapley_values, feature_values, feature_names):
-    """Wrap Shapley values in an `Explanation` object."""
-    return shap.Explanation(
-        # TODO: does this base value make sense? We could always get the
-        # model outputs by updating the analysis.
-        base_values=0.0,
-        values=shapley_values,
-        data=feature_values,
-        feature_names=feature_names,
-    )
-
-
-def process_file(
-    filename,
-    ignore_indicators_and_counts=False,
-    hours_before=None,
-):
-    """Process file.
-
-    Parameters
-    ----------
-    filename : str
-        Input filename.
-
-    ignore_indicators_and_counts : bool, optional
-        If set, ignores indicator and count features, thus reducing the
-        number of features considered.
-
-    hours_before : int or `None`, optional
-        If not `None`, only uses (at most) the last `hours_before`
-        observations when reporting the Shapley values.
-
-    Returns
-    -------
-    Tuple of pooled Shapley values, features, feature names, and data
-    set name.
-    """
-    with open(filename, 'rb') as f:
-        data = pickle.load(f)
-
-    _, _, out = get_model_and_dataset(
-        get_run_id(filename), return_out=True
-    )
-
-    dataset_name = out['model_params']['dataset']
-    model_name = out['model']
-
-    assert model_name == 'AttentionModel', RuntimeError(
-        'Shapley analysis is currently only supported for the '
-        'AttentionModel class.'
-    )
-
-    feature_names = data['feature_names']
-    lengths = data['lengths'].numpy()
-
-    if args.ignore_indicators_and_counts:
-        keep_features = [
-            True if not col.endswith('indicator') and not col.endswith('count')
-            else False
-            for col in feature_names
-        ]
-
-        important_indices = np.where(keep_features)[0]
-        selected_features = np.array(feature_names)[important_indices]
-    else:
-        important_indices = np.arange(0, len(feature_names))
-        selected_features = feature_names
-
-    # ignore positional encoding
-    important_indices += 10
-
-    # Tensor has shape `n_samples, max_length, n_features`, where
-    # `max_length` denotes the maximum length of a time series in
-    # the batch.
-    shap_values = data['shap_values']
-    shap_values = shap_values[:, :, important_indices]
-    features = data['input'].numpy()
-    features = features[:, :, important_indices]
-
-    shap_values_pooled, features_pooled = pool(
-        lengths,
-        shap_values,
-        features,
-        hours_before,
-    )
-
-    return (shap_values_pooled,
-            features_pooled,
-            selected_features,
-            dataset_name)
+from src.torch.shap_utils import get_pooled_shapley_values
 
 
 def make_plots(
@@ -218,8 +16,6 @@ def make_plots(
     feature_values,
     feature_names,
     dataset_name,
-    collapse_features=False,
-    aggregation_function='max',
     prefix='',
     out_dir=None,
 ):
@@ -240,14 +36,6 @@ def make_plots(
     dataset_name : str
         Name of the data set for which the visualisations are being
         prepared. Will be used to generate filenames.
-
-    collapse_features : bool, optional
-        If set, collapse features into their corresponding variables,
-        and aggregate their respective values.
-
-    aggregation_function : str, optional
-        Sets aggregation function for feature collapse operation. Will
-        only be used if `collapse_features == True`.
 
     prefix : str, optional
         If set, adds a prefix to all filenames. Typically, this prefix
@@ -279,44 +67,6 @@ def make_plots(
         plt.savefig(filename_prefix + f'_{plot}.png', dpi=300)
         plt.clf()
 
-    # Optional filtering and merging over the columns, as specified by
-    # the user. This permits us to map features to their corresponding
-    # variables.
-    if collapse_features:
-        df = pd.DataFrame(shap_values, columns=feature_names)
-
-        def feature_to_var(column):
-            """Rename feature name to variable name."""
-            column = column.replace('_count', '')
-            column = column.replace('_raw', '')
-            column = column.replace('_indicator', '')
-            return column
-
-        aggregation_fn = get_aggregation_function(aggregation_function)
-
-        print(
-            f'Collapsing features to variables using '
-            f'{aggregation_function}...'
-        )
-
-        df = df.rename(feature_to_var, axis=1)
-        df = df.groupby(level=0, axis=1).apply(
-            lambda x: x.apply(aggregation_fn, axis=1)
-        )
-
-        shap_values = df.to_numpy()
-        feature_names = df.columns
-
-        shap.summary_plot(
-            shap_values,
-            feature_names=feature_names,
-            plot_type='bar',
-            show=False,
-        )
-        plt.tight_layout()
-        plt.savefig(filename_prefix + '_variables_bar.png', dpi=300)
-        plt.clf()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -332,21 +82,6 @@ if __name__ == '__main__':
         '-i', '--ignore-indicators-and-counts',
         action='store_true',
         help='If set, ignores indicator and count features.'
-    )
-
-    parser.add_argument(
-        '-c', '--collapse-features',
-        action='store_true',
-        help='If set, use only variables, collapsing features according '
-             'to the aggregation function.'
-    )
-
-    parser.add_argument(
-        '-a', '--aggregation-function',
-        choices=['absmax', 'max', 'mean', 'median', 'min'],
-        default='absmax',
-        help='Aggregation function to use when features are collapsed to '
-             'variables.'
     )
 
     parser.add_argument(
@@ -375,7 +110,7 @@ if __name__ == '__main__':
 
     for filename in args.FILE:
         shap_values, feature_values, feature_names, dataset_name = \
-            process_file(
+            get_pooled_shapley_values(
                 filename,
                 args.ignore_indicators_and_counts,
                 args.hours_before,
@@ -410,6 +145,4 @@ if __name__ == '__main__':
         feature_names,
         dataset_name,
         prefix=prefix,
-        collapse_features=args.collapse_features,
-        aggregation_function=args.aggregation_function,
     )
