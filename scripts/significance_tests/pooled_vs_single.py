cfe7066cc290423a2f576299f18400ed3c12ecd7
mi92 <michael.d.moor@gmail.com>
2021-08-09 09:09:45 +0200

    updated sig test script

--- scripts/significance_tests/pooled_vs_single.py
+++ scripts/significance_tests/pooled_vs_single.py
@@ -1,7 +1,9 @@
-import scipy 
+import scipy.stats as stats 
 import numpy as np
 import pandas as pd
 from IPython import embed
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def main():
     # hard coded result files:
@@ -24,7 +26,7 @@ def main():
     datasets = ['aumc', 'eicu', 'hirid', 'mimic']
 
     # emory is reported seperately:
-    df = df.query("train_dataset != 'emory' & eval_dataset != 'emory'")
+    df = df.query("train_dataset != 'emory'  & eval_dataset != 'emory' & model == 'AttentionModel'")
     
     # for each evaluation site (core datasets),
     # get paired results: pooled vs mean of single dataset perf.
@@ -33,7 +35,39 @@ def main():
     # get single dataset results:
     df_s = df.query("train_dataset != 'pooled' & train_dataset != eval_dataset")
 
-    embed() 
+    #first get means of single dataset performances (expected performance on single dataset)
+    means = []
+    for dataset, df_ in df_s.groupby('eval_dataset'):
+        curr_means = df_.groupby(['rep','subsample']).max() #mean() 
+        curr_means['eval_dataset'] = dataset
+        means.append(curr_means)
+    df_means = pd.concat(means)
     
+    # iterate over datasets and run the test:
+    for dataset in datasets:
+        mu = df_means.query("eval_dataset == @dataset")
+        pooled = df_p.query("eval_dataset == @dataset")
+        #this step is merely to sort and arrange the comparison array similarly 
+        pooled = pooled.groupby(['rep','subsample']).max() 
+        
+        print('>>> ',dataset)
+        print('Shapiro-Wilk test for normality:')
+        print('Pooled AUCs:', stats.shapiro(pooled['AUC']))
+        print('Mean Single AUCs:', stats.shapiro(mu['AUC']))
+        print('Paired t-test:')
+        print(stats.ttest_rel(mu['AUC'], pooled['AUC']))
+        print('Wilcoxon signed rank test:')
+        wc = stats.wilcoxon(mu['AUC'], pooled['AUC'])
+        print(stats.wilcoxon(mu['AUC'], pooled['AUC']))
+
+        plt.figure()
+        plt.title(f' {dataset}, AUC values: best single vs pooled, wilcoxon p = {wc.pvalue:.2E}')
+        plt.hist(mu['AUC'], label='best single', alpha=0.5)
+        plt.hist(pooled['AUC'], label='pooled', alpha=0.5)
+        plt.legend()
+        plt.ylabel('counts')
+        plt.xlabel('AUC')
+        plt.savefig(f'results/evaluation_test/plots/pooled_vs_single_wc_test_{dataset}.png')
+
 if __name__ == "__main__":
     main()
