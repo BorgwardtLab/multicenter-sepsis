37af0e8536a11ea49d3ab0ae464f7ba89dbda4b0
mi92 <michael.d.moor@gmail.com>
2022-09-19 07:01:00 +0200

    updated script for creating larger ft split

--- revisions/finetune/splits/create_large_finetuning_splits.py
+++ revisions/finetune/splits/create_large_finetuning_splits.py
@@ -19,8 +19,7 @@ def main():
     #test_sizes = [0.5, 0.8] #corresponds to 0.5 and 0.2 train_size
     finetuning_sizes = np.array([0.2,0.3,0.4,0.5]) # for comparability with ft < 10% we 
     # reuse validation split + train split for remainder
-    portion_from_train_split = finetuning_sizes - 0.1 # percentage of overall dataset used from trainn split
-    ratio_of_train_split = portion_from_train_split / 0.8 # due to train spli making up 80%
+    ratio_of_train_split = finetuning_sizes / 0.8 # due to train spli making up 80%
     test_sizes = 1- ratio_of_train_split # test_size to apply on train split 
     split_path = 'config/splits' 
 
@@ -39,18 +38,14 @@ def main():
         # we subdivide starting from the split_0 train split:
         ids_start = d['dev']['split_0']['train'] 
         labels_start = [id_to_labels[x] for x in ids_start]
-        # and addd val split to each newly created split (to be comparable with ft splits created from val split)
-        ids_val = d['dev']['split_0']['validation'] 
-        labels_val = [id_to_labels[x] for x in ids_val]
-
+        
         for test_size, ft_size in zip(test_sizes, finetuning_sizes):
             print(f'Creating split for test size {test_size}')
             sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=0)
             sss.get_n_splits(labels_start)
             for train_index, test_index in sss.split(labels_start,labels_start):
                 train_ids = np.array(ids_start)[train_index]
-                train_ids = ids_val + train_ids.tolist()
-                d['dev']['split_0'][f'finetuning_train_size_{ft_size*10}'] = train_ids
+                d['dev']['split_0'][f'finetuning_train_size_{ft_size*10}'] = train_ids.tolist()
                 l = [id_to_labels[x] for x in train_ids]
                 print(np.sum(l) / len(l))
                 # had to round due to weird display of 0.2 as 0.1999..
