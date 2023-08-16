70e68cb75fbf8f085126ede906178e275059cd17
mi92 <michael.d.moor@gmail.com>
2023-08-16 12:52:14 -0700

    further cleanup

--- scripts/replicate_best_estimators.sh
+++
@@ -1,7 +0,0 @@
-for DATASET in 'aumc' 'eicu' 'hirid' 'mimic' 'physionet2019'; do
-  NAME=${DATASET}_lr
-  scp ../results/${NAME}/best_estimator.pkl \
-      ../results/${NAME}/cv_results.csv     \
-      ../results/${NAME}/results.json       \
-      bs-borgwardt02:/links/groups/borgwardt/Projects/sepsis/multicenter-sepsis/results/${NAME}_euler
-done
